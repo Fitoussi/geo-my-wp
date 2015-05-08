5ff7f7a329dea79a1190d5ea045b1b253b749dce
Eyal Fitoussi <info@geomywp.com>
2015-05-07 21:33:33 -1000

    - update: GEO my WP main map script was completely re-written for improved performance. GEO my WP will now proccess all maps of forms, shortcode, add-ons and so on using this map script.

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,75 +2,60 @@
  * GMW main map function
  * @param gmwForm
  */
-function gmwMapInit( gmwForm ) {
+function gmwMapInit( values ) {
+	
+	//make sure the map element exists to prevent JS error
+	if ( !jQuery( '#'+values['mapElement'] ).length ) {
+		return;
+	}
 
-	//create global maps object if was not created already
-	if ( typeof gmwMapObjects == 'undefined' ) {
-		gmwMapObjects = {};
-	}
-	
-	var i;
-	var formId			= gmwForm.ID;
-	var gmwMapMarkers  	= [];
-	var zoomLevel  		= ( gmwForm.results_map['zoom_level'] == 'auto' ) ? 13 : gmwForm.results_map['zoom_level'];
-	var gmwUserLocation = false;
-	var latlngbounds 	= new google.maps.LatLngBounds();
-	
-	//pass global mapObject to be used outside this file
-	gmwMapObjects[formId] = {
-			form:gmwForm,
-			mapId:formId,
-			mapElement:'gmw-map-'+formId,
-			mapType:gmwForm.prefix + 'Map'
-	};
-		    
-	//map options
+	//map id
+	var mapID = values.mapId;
+	var markersClustereOk = false;
+
+	//initiate map options
 	var mapOptions = {
-		zoom: parseInt(zoomLevel),
-		draggable: true,
-		panControl: true,
-  		zoomControl: true,
-  		mapTypeControl: true,
-  		mapTypeControlOptions: {
-	        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-	        position: google.maps.ControlPosition.TOP_RIGHT
-	    },
-	    center: new google.maps.LatLng( gmwForm.your_lat,gmwForm.your_lng ),
-		mapTypeId: google.maps.MapTypeId[gmwForm.results_map['map_type']],
-		styles: gmwMapObjects[formId]['mapStyle']
-	};			
+			zoom: ( values['zoomLevel'] == 'auto' ) ? 13 : parseInt(values['zoomLevel']),
+			center: new google.maps.LatLng( values['userPosition']['lat'], values['userPosition']['lng'] ),
+			mapTypeId: google.maps.MapTypeId[values.mapTypeId]
+	};
 	
-	//map styles
-	gmwMapOptions = ( "undefined" != typeof gmwMapOptions ) ? gmwMapOptions : {};
-	if ( "undefined" != typeof gmwMapOptions[formId] ) {
-		jQuery.extend(mapOptions, gmwMapOptions[formId] );
+	//merge custom map options if exsits
+	if ( "undefined" != typeof gmwMapOptions[mapID] ) {
+		jQuery.extend(mapOptions, gmwMapOptions[mapID] );
 	}
+			
+	//merge map options with global map object
+	gmwMapObjects[mapID] = jQuery.extend( true, values, {
+			mapOptions: mapOptions,	
+			bounds: new google.maps.LatLngBounds(),
+			markers:[],
+			infoWindow:false,
+			resizeMapControl:false
+	} );
 	
 	//create the map
-	gmwMapObjects[formId]['map'] = new google.maps.Map(document.getElementById( 'gmw-map-' + formId ), mapOptions );
-	
-	//initiate markers clusterer or spiderfier if needed
-	if (  "undefined" == typeof gmwForm.results_map.markers_display || gmwForm.results_map.markers_display != 'markers_clusterer' ) {  	
-		gmwForm.results_map.markers_display = 'none';
-	}
-	
-    if ( gmwForm.results_map.markers_display == 'markers_clusterer' ) {  	 
-		 psMc = new MarkerClusterer( gmwMapObjects[formId]['map'], gmwMapMarkers );	
+	gmwMapObjects[mapID]['map'] = new google.maps.Map(document.getElementById( gmwMapObjects[mapID]['mapElement'] ), gmwMapObjects[mapID]['mapOptions'] );
+
+	//initiate markers clusterer if needed ( for premium features )
+	if ( typeof MarkerClusterer === 'function' && gmwMapObjects[mapID]['markersDisplay'] == 'markers_clusterer' ) { 
+		psMc = new MarkerClusterer( gmwMapObjects[mapID]['map'], gmwMapObjects[mapID]['markers'] );	
+		markersClustereOk = true;
     }
-    
+
 	//create markers for locations
-	for ( i = 0; i < gmwForm.results.length; i++ ) {  
+	for ( i = 0; i < gmwMapObjects[mapID]['locations'].length; i++ ) {  
 		
-		//if ( gmwForm.results[i]['lat'] == null || gmwForm.results[i]['long'] == null || gmwForm.results[i]['lat'] == '0.000000' || gmwForm.results[i]['long'] == '0.000000' )
-			//continue;
+		//make sure location has coordinates to prevent JS error
+		if ( gmwMapObjects[mapID]['locations'][i]['lat'] == undefined || gmwMapObjects[mapID]['locations'][i]['long'] == undefined  )
+			continue;
 		
-		var gmwLocation;
-		gmwLocation = new google.maps.LatLng( gmwForm.results[i]['lat'], gmwForm.results[i]['long'] );
+		var gmwLocation = new google.maps.LatLng( gmwMapObjects[mapID]['locations'][i]['lat'], gmwMapObjects[mapID]['locations'][i]['long'] );
 	
 		//offset markers with same location
-		if ( latlngbounds.contains(gmwLocation) ) {
+		if ( markersClustereOk == false && gmwMapObjects[mapID]['bounds'].contains(gmwLocation) ) {
 
-			var a = 360.0 / gmwForm.results.length;
+			var a = 360.0 / gmwMapObjects[mapID]['locations'].length;
 			var orgPosition = gmwLocation;
 	        var newLat = orgPosition.lat() + -.0005 * Math.cos((+a*i) / 180 * Math.PI);  // x
 	        var newLng = orgPosition.lng() + -.0005 * Math.sin((+a*i) / 180 * Math.PI);  // Y
@@ -83,121 +68,153 @@ function gmwMapInit( gmwForm ) {
 	            strokeWeight: 2
 	          });
 	        
-	        markerPath.setMap(gmwMapObjects[formId]['map']);
+	        markerPath.setMap(gmwMapObjects[mapID]['map']);
 		}
 		
-        latlngbounds.extend(gmwLocation);
-        
-		//console.log(gmwForm.results[i]);		
-		gmwMapMarkers[i] = new google.maps.Marker({
+		gmwMapObjects[mapID]['bounds'].extend(gmwLocation);
+        	
+		gmwMapObjects[mapID]['markers'][i] = new google.maps.Marker({
 			position: gmwLocation,
-			icon:gmwForm.results[i]['mapIcon'],
-			map:gmwMapObjects[formId]['map'],
+			icon:gmwMapObjects[mapID]['locations'][i]['mapIcon'],
+			map:gmwMapObjects[mapID]['map'],
 			id:i 
 		});
 				
-		 //add marker to clustere if needed
-		if ( gmwForm.results_map.markers_display == 'markers_clusterer' )	{						
-			psMc.addMarker( gmwMapMarkers[i] );							
+		 //add marker to clusterer if needed
+		if ( gmwMapObjects[mapID]['markersDisplay'] == 'markers_clusterer' ) {						
+			psMc.addMarker( gmwMapObjects[mapID]['markers'][i] );							
 		} else {		
-			gmwMapMarkers[i].setMap(gmwMapObjects[formId]['map']);			
+			gmwMapObjects[mapID]['markers'][i].setMap(gmwMapObjects[mapID]['map'] );			
 		}
 		
-		with ({ gmwMapMarker: gmwMapMarkers[i] }) {
-			var ptiw = false;
-			google.maps.event.addListener( gmwMapMarker, 'click', function() {
-				if (ptiw) {
-					ptiw.close();
-					ptiw = null;
+		if ( gmwMapObjects[mapID]['locations'][i]['info_window_content'] != false ) {
+			
+			//create the info-window object
+			google.maps.event.addListener(gmwMapObjects[mapID]['markers'][i], 'click', function() {
+				
+				if ( gmwMapObjects[mapID]['infoWindow'] ) {
+					gmwMapObjects[mapID]['infoWindow'].close();
+					gmwMapObjects[mapID]['infoWindow'] = null;
 				}
-				ptiw = new google.maps.InfoWindow({
-					content: gmwForm.results[this.id].info_window_content
+				
+				gmwMapObjects[mapID]['infoWindow'] = new google.maps.InfoWindow({
+					content: gmwMapObjects[mapID]['locations'][this.id]['info_window_content']
 				});
-				ptiw.open( gmwMapObjects[formId]['map'], gmwMapMarker );	 		
+				
+				gmwMapObjects[mapID]['infoWindow'].open(gmwMapObjects[mapID]['map'], this);
 			});
 		}
 	}
+				
+	//create user's location marker
+	if ( gmwMapObjects[mapID]['userPosition']['lat'] != false && gmwMapObjects[mapID]['userPosition']['lng'] != false && gmwMapObjects[mapID]['userPosition']['mapIcon'] != false ) {
 	
-	gmwMapObjects[formId]['markers'] = gmwMapMarkers;
-	
-	//create user's current location
-	if ( gmwForm.your_lat != false && gmwForm.your_lng != false ) {
-	
-		gmwUserLocation  = new google.maps.LatLng( gmwForm.your_lat,gmwForm.your_lng );
-		latlngbounds.extend(gmwUserLocation);
+		//user's location
+		gmwMapObjects[mapID]['userPosition']['location'] = new google.maps.LatLng( gmwMapObjects[mapID]['userPosition']['lat'], gmwMapObjects[mapID]['userPosition']['lng'] );
 		
-		var yliw = new google.maps.InfoWindow({
-			content: gmwForm.labels.info_window.your_location
-		});
-	      
-		ulMarker = new google.maps.Marker({
-			position: new google.maps.LatLng( gmwForm.your_lat,gmwForm.your_lng ),
-			map: gmwMapObjects[formId]['map'],
-			icon:gmwForm.ul_icon
+		//append user's location to bounds
+		gmwMapObjects[mapID]['bounds'].extend(gmwMapObjects[mapID]['userPosition']['location']);
+		
+		//create user's marker
+		gmwMapObjects[mapID]['userPosition']['marker'] = new google.maps.Marker({
+			position: gmwMapObjects[mapID]['userPosition']['location'],
+			map: gmwMapObjects[mapID]['map'],
+			icon:gmwMapObjects[mapID]['userPosition']['mapIcon']
 		});
 		
-		gmwMapObjects[formId]['userLocation'] = ulMarker;
+		//create user's marker info-window
+		if ( gmwMapObjects[mapID]['userPosition']['iwContent'] != null ) {
 		
-		if ( gmwForm.results_map.yl_icon != undefined && gmwForm.results_map.yl_icon == 1 ) {
-			yliw.open( gmwMapObjects[formId]['map'], ulMarker);
+			var iw = new google.maps.InfoWindow({
+				content: gmwMapObjects[mapID]['userPosition']['iwContent']
+			});
+		      					
+			if ( gmwMapObjects[mapID]['userPosition']['iwOpen'] == true ) {
+				iw.open( gmwMapObjects[mapID]['map'], gmwMapObjects[mapID]['userPosition']['marker']);
+			}
+			
+		    google.maps.event.addListener( gmwMapObjects[mapID]['userPosition']['marker'], 'click', function() {
+		    	iw.open( gmwMapObjects[mapID]['map'], gmwMapObjects[mapID]['userPosition']['marker']);
+		    });     
+		}
+	}
+					
+	//after map loaded
+	google.maps.event.addListenerOnce(gmwMapObjects[mapID]['map'], 'idle', function(){	
+
+		//custom zoom point
+		if ( gmwMapObjects[mapID]['zoomPosition'] != false && gmwMapObjects[mapID]['zoomLevel'] != 'auto' ) {
+
+			gmwMapObjects[mapID]['zoomPosition']['position'] = new google.maps.LatLng( gmwMapObjects[mapID]['zoomPosition']['lat'], gmwMapObjects[mapID]['zoomPosition']['lng'] );
+			gmwMapObjects[mapID]['map'].setZoom( parseInt( gmwMapObjects[mapID]['zoomLevel'] ) );
+			gmwMapObjects[mapID]['map'].panTo( gmwMapObjects[mapID]['zoomPosition']['position'] );
+
+		} else if ( gmwMapObjects[mapID]['locations'].length == 1 && gmwMapObjects[mapID]['userPosition']['location'] == false ) {
+
+			gmwMapObjects[mapID]['map'].setZoom(13);
+			gmwMapObjects[mapID]['map'].panTo(gmwMapObjects[mapID]['markers'][0].getPosition());	
+
+		} else if ( gmwMapObjects[mapID]['zoomLevel'] != 'auto' && gmwMapObjects[mapID]['userPosition']['location'] !== false ) {
+
+			gmwMapObjects[mapID]['map'].setZoom( parseInt( gmwMapObjects[mapID]['zoomLevel'] ) );
+			gmwMapObjects[mapID]['map'].panTo(gmwMapObjects[mapID]['userPosition']['location']);
+
+		} else if ( gmwMapObjects[mapID]['zoomLevel'] == 'auto' || gmwMapObjects[mapID]['userPosition']['location'] == false  ) { 
+
+			gmwMapObjects[mapID]['map'].fitBounds(gmwMapObjects[mapID]['bounds']);
 		}
 		
-	    google.maps.event.addListener( ulMarker, 'click', function() {
-	    	yliw.open( gmwMapObjects[formId]['map'], ulMarker);
-	    });       
-	}
-	
-	gmwMapObjects[formId]['bonds'] = latlngbounds;
-				
-	//zoom map based on settings
-	if ( gmwForm.results.length == 1 && gmwUserLocation == false ) {
-		gmwMapObjects[formId]['map'].setZoom(13);
-		gmwMapObjects[formId]['map'].panTo(gmwMapMarkers[0].getPosition());	
-	} else if ( gmwForm.results_map.zoom_level != 'auto' && gmwUserLocation !== false ) {
-		gmwMapObjects[formId]['map'].setZoom(parseInt(gmwForm.results_map.zoom_level));
-		gmwMapObjects[formId]['map'].panTo(gmwUserLocation);
-	} else if ( gmwForm.results_map.zoom_level == 'auto' || gmwUserLocation == false  ) { 
-		gmwMapObjects[formId]['map'].fitBounds(latlngbounds);
-	}
-	
-	google.maps.event.addListenerOnce(gmwMapObjects[formId]['map'], 'idle', function(){	
+		//fadeout the map loader if needed
+		if ( gmwMapObjects[mapID]['mapLoaderElement'] != false ) {
+			jQuery(gmwMapObjects[mapID]['mapLoaderElement']).fadeOut(1000);
+		}
 		
-		//create map expand toggle
-		ExpandControl = document.getElementById('gmw-expand-map-trigger-' + formId);
-		gmwMapObjects[formId]['map'].controls[google.maps.ControlPosition.TOP_RIGHT].push(ExpandControl);			
-		ExpandControl.style.display = 'block';
-	
-		//expand map function
-	    if ( jQuery('#gmw-expand-map-trigger-'+formId).length ) {
-	    	
-	    	jQuery('#gmw-expand-map-trigger-'+formId).click(function() {
+		//create map expand toggle if needed
+		if ( gmwMapObjects[mapID]['resizeMapElement'] != false ) {
+			
+			gmwMapObjects[mapID]['resizeMapControl'] = document.getElementById(gmwMapObjects[mapID]['resizeMapElement']);
+			gmwMapObjects[mapID]['resizeMapControl'].style.position = 'absolute';	
+			gmwMapObjects[mapID]['map'].controls[google.maps.ControlPosition.TOP_RIGHT].push(gmwMapObjects[mapID]['resizeMapControl']);			
+			gmwMapObjects[mapID]['resizeMapControl'].style.display = 'block';
+		
+			//expand map function		    	
+	    	jQuery('#'+gmwMapObjects[mapID]['resizeMapElement']).click(function() {
 	    		
-	    		var mapCenter = gmwMapObjects[formId]['map'].getCenter();
+	    		var mapCenter = gmwMapObjects[mapID]['map'].getCenter();
 	    		jQuery(this).closest('.gmw-map-wrapper').toggleClass('gmw-expanded-map');          		
-	    		jQuery(this).toggleClass('dashicons-editor-expand').toggleClass('dashicons-editor-contract');
+	    		jQuery(this).toggleClass('fa-expand').toggleClass('fa-compress');
 	    		
-	    		setTimeout(function() { 
-	    		
-	    			google.maps.event.trigger(gmwMapObjects[formId]['map'], 'resize');
-	    			gmwMapObjects[formId]['map'].setCenter(mapCenter);
-					
+	    		setTimeout(function() { 			    		
+	    			google.maps.event.trigger(gmwMapObjects[mapID]['map'], 'resize');
+	    			gmwMapObjects[mapID]['map'].setCenter(mapCenter);							
 				}, 100);            		
 	    	});
-	    }
+		}
 	});
 }
 
-jQuery(document).ready(function($){ 		
-	//check that map element exists before doing anything
-	if ( ( gmwForm.page_load_results_trigger || gmwForm.auto_results_trigger || gmwForm.submitted  ) && $('#gmw-map-wrapper-'+gmwForm.ID).length ) {	
+jQuery(document).ready(function($){ 	
+	
+	if ( typeof gmwMapsHolder != 'undefined' ) {
 		
-		//if map element is hidden show it first
-		if ( jQuery('#gmw-map-wrapper-'+gmwForm.ID).is(':hidden') ) {
-			jQuery( '#gmw-map-wrapper-'+gmwForm.ID ).slideToggle( 'fast', function() {
-				gmwMapInit( gmwForm );
-			});
-		} else {
-			gmwMapInit( gmwForm );
-		} 
+		//create global maps object if was not created already
+		if ( typeof gmwMapObjects == 'undefined' ) {
+			gmwMapObjects = {};
+		}
+				
+		$.each(gmwMapsHolder, function( index, values ) {
+			
+			if ( values['triggerMap'] == true ) {
+			
+				//if map element is hidden show it first
+				if ( gmwMapsHolder[index]['hiddenElement'] != false && jQuery(gmwMapsHolder[index]['hiddenElement']).is(':hidden') ) {
+					jQuery(gmwMapsHolder[index]['hiddenElement']).slideToggle( 'fast', function() {
+						gmwMapInit( values );
+					});
+				} else {
+					gmwMapInit( values );
+				} 	
+			}
+		});		
 	}
 });
