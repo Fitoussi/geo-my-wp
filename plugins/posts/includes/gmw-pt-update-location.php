ec3642b4ff8377c5659a2dec07d4faa206eb5b23
Eyal Fitoussi <info@geomywp.com>
2015-05-11 11:15:38 -1000

    Update post location function replace wp_parse_args with array_replace_recursive to prevent empty field ( additional_info ). delete location from database if addrress failed geocoding. varius bug fix

--- plugins/posts/includes/gmw-pt-update-location.php
+++ plugins/posts/includes/gmw-pt-update-location.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * GMW PT function - add location to GEO my WP 
  * 
@@ -8,81 +7,93 @@
  * function accepts an accociative array as below:
  * 
  * $args = array (
- *                  'post_id'          => false, 	//must pass post id in order to work
- *                  'address'           => false,	//can be eiter single line of full address field or an array of the adress components ex ( array( 'street' => '5060 lincoln st', 'city' => 'hollywood', 'state' => 'florida' )
- *                  'additional_info'   => array( 'phone' => false, 'fax' => false, 'email' => false, 'website' => false ),
- *                  'map_icon'          => false
- *  		);
+ *       'post_id'           => false, 	//must pass post id in order to work
+ *       'address'           => false,	//can be eiter single line of full address field or an array of the adress components ex ( array( 'street' => '5060 lincoln st', 'city' => 'hollywood', 'state' => 'florida' )
+ *       'additional_info'   => array( 'phone' => false, 'fax' => false, 'email' => false, 'website' => false ),
+ *       'map_icon'          => false
+ *  );
  */
 function gmw_pt_update_location( $args ) {
 
-    $defaults = array(
+    $args = array_replace_recursive( array(
         'post_id'         => false,
         'address'         => false,
         'additional_info' => array( 'phone' => false, 'fax' => false, 'email' => false, 'website' => false ),
         'map_icon'        => '_default.png',
-    );
+    ), $args );
 
-    $r = wp_parse_args( $args, $defaults );
-
-    extract( $r );
-
-    if ( empty( $post_id ) || empty( $address ) )
+    if ( empty( $args['post_id'] ) || empty( $args['address'] ) )
         return;
 
-    if ( empty( $map_icon ) ) {
-        $map_icon = '_default.png';
+    if ( empty( $args['map_icon'] ) ) {
+        $args['map_icon'] = '_default.png';
     }
-    
-    //if multiple address fields
-    if ( is_array( $address ) ) {
 
-        $address_apt = implode( ' ', $address );
+    $address_apt     = $address = $args['address'];
+    $multiple_fields = false;
 
-        unset( $address['apt'] );
+    if ( is_array( $args['address'] ) ) {
+        
+        $mulitple_field = true;
+        $address_apt    = implode( ' ', $address );
+
+        unset( $address[ 'apt' ] );
 
         $address = implode( ' ', $address );
+    }
 
-        $returned_address = GEO_my_WP::geocoder( $address );
+    //geocode the address
+    $geocoded_address = GEO_my_WP::geocoder( $address );
 
+    //if geocode failed delete the user's location
+    if ( isset( $geocoded_address['error'] ) ) {
+        
+        global $wpdb;
+
+        $wpdb->query( $wpdb->prepare( "DELETE FROM {$wpdb->prefix}places_locator WHERE `post_id` = %d", array( $args['post_id'] ) ) );
+
+        do_action('gmw_pt_udpate_location_after_location_deleted', $args, $geocoded_address );
+
+        return;
+    }
+
+    //if multiple address field passed through array
+    if ( $multiple_fields == true ) {
+    
         if ( !in_array( 'street', $args['address'] ) || empty( $args['address']['street'] ) ) {
-        	$street  = $returned_address['street'];
+        	$street  = $geocoded_address['street'];
         }
         
         if ( !in_array( 'apt', $args['address'] ) || empty( $args['address']['apt'] ) ) {
-        	$apt = $returned_address['apt'];
+        	$apt = $geocoded_address['apt'];
         }
         
         if ( !in_array( 'city', $args['address'] ) || empty( $args['address']['city'] ) ) {
-        	$city = $returned_address['city'];
+        	$city = $geocoded_address['city'];
         }
                
         if ( !in_array( 'zipcode', $args['address'] ) || empty( $args['address']['zipcode'] ) ) {
-        	$zipcode = $returned_address['zipcode'];
+        	$zipcode = $geocoded_address['zipcode'];
         }
         
-        $state   = $returned_address['state_short'];
-        $country = $returned_address['country_short'];
+        $state   = $geocoded_address['state_short'];
+        $country = $geocoded_address['country_short'];
 
     } else {
 
-        $address     = $address;
-        $address_apt = $address;
-
-        $returned_address = GEO_my_WP::geocoder( $address );
-
-        $street  = $returned_address['street'];
-        $apt     = $returned_address['apt'];
-        $city    = $returned_address['city'];
-        $state   = $returned_address['state_short'];
-        $zipcode = $returned_address['zipcode'];
-        $country = $returned_address['country_short'];
+        $street  = $geocoded_address['street'];
+        $apt     = $geocoded_address['apt'];
+        $city    = $geocoded_address['city'];
+        $state   = $geocoded_address['state_short'];
+        $zipcode = $geocoded_address['zipcode'];
+        $country = $geocoded_address['country_short'];
 
     }
 
     $locationArgs = array(
         'args'     => $args,
-        'geocoded' => $returned_address
+        'address'  => $address,
+        'geocoded' => $geocoded_address
     );
 
     $LocationArgs = apply_filters( 'gmw_pt_before_location_updated', $locationArgs );
@@ -90,16 +101,17 @@ function gmw_pt_update_location( $args ) {
     do_action( 'gmw_pt_before_location_updated', $locationArgs );
 
     //$featuredPost = ( isset( $_POST['_wppl_featured_post'] ) ) ? $_POST['_wppl_featured_post'] : 0;
+    
     //Save information to database
     global $wpdb;
     $wpdb->replace( $wpdb->prefix . 'places_locator', array(
     		'post_id'           => $locationArgs['args']['post_id'],
     		'feature'           => 0,
     		'post_type'         => get_post_type( $locationArgs['args']['post_id'] ),
-    		'post_title'        => stripslashes( get_the_title( $locationArgs['args']['post_id'] ) ),
+    		'post_title'        => get_the_title( $locationArgs['args']['post_id'] ),
     		'post_status'       => 'publish',
-    		'street_number'    	=> $locationArgs['geocoded']['street_number'],
-    		'street_name'       => $locationArgs['geocoded']['street_name'],
+    		'street_number'    	=> ( !empty( $locationArgs['geocoded']['street_number'] ) ) ? $locationArgs['geocoded']['street_number'] : '',
+    		'street_name'       => ( !empty( $locationArgs['geocoded']['street_name'] ) ) ? $locationArgs['geocoded']['street_name'] : '',
     		'street'            => $street,
     		'apt'               => $apt,
     		'city'              => $city,
@@ -110,15 +122,13 @@ function gmw_pt_update_location( $args ) {
     		'country_long'      => $locationArgs['geocoded']['country_long'],
     		'address'           => $address_apt,
     		'formatted_address' => $locationArgs['geocoded']['formatted_address'],
-    		'phone'             => $locationArgs[ 'args' ][ 'additional_info' ][ 'phone' ],
-    		'fax'               => $locationArgs[ 'args' ][ 'additional_info' ][ 'fax' ],
-    		'email'             => $locationArgs[ 'args' ][ 'additional_info' ][ 'email' ],
-    		'website'           => $locationArgs[ 'args' ][ 'additional_info' ][ 'website' ],
-    		'lat'               => $locationArgs[ 'geocoded' ][ 'lat' ],
-    		'long'              => $locationArgs[ 'geocoded' ][ 'lng' ],
-    		'map_icon'          => $map_icon,
-    )
-    );
-
+    		'phone'             => $locationArgs['args']['additional_info']['phone'],
+    		'fax'               => $locationArgs['args']['additional_info']['fax'],
+    		'email'             => $locationArgs['args']['additional_info']['email'],
+    		'website'           => $locationArgs['args']['additional_info']['website'],
+    		'lat'               => $locationArgs['geocoded']['lat'],
+    		'long'              => $locationArgs['geocoded']['lng'],
+    		'map_icon'          => $args['map_icon'],
+    ));
     do_action( 'gmw_pt_after_location_updated', $locationArgs );
 }
