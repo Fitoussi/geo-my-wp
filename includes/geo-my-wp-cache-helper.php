24f59685938c4c464403109f1fa27fca98315bf8
Eyal Fitoussi <info@geomywp.com>
2015-08-18 18:13:19 -1000

    New GEO_my_WP_Cache_Helper class ( based a class taken from WP Job Manager by Mike Jolly's. Thank you! ). The class will help cache "expensive" databse queries such as terms, taxonomies, search results and more in transient. That is to improve performance.

--- includes/geo-my-wp-cache-helper.php
+++ includes/geo-my-wp-cache-helper.php
@@ -2,32 +2,42 @@
 /**
  * GEO_my_WP_Cache_Helper class.
  *
- * This AWESOME calss and idea was "stolen" from WP JOb Manager plugin developed by Mike Jolley. Thank you!
+ * This AWESOME class and idea was "stolen" from WP JOb Manager plugin developed by Mike Jolley. Thank you!
  */
 class GEO_my_WP_Cache_Helper {
 
 	public static function init() {
-		add_action( 'save_post', array( __CLASS__, 'flush_get_job_listings_cache' ) );
+		//add_action( 'save_post', array( __CLASS__, 'flush_get_job_listings_cache' ) );
 		add_action( 'set_object_terms', array( __CLASS__, 'set_term' ), 10, 4 );
+		add_action( 'edited_term', array( __CLASS__, 'edited_term' ), 10, 3 );
+		add_action( 'create_term', array( __CLASS__, 'edited_term' ), 10, 3 );
+		add_action( 'delete_term', array( __CLASS__, 'edited_term' ), 10, 3 );
 		add_action( 'gmw_clear_expired_transients', array( __CLASS__, 'clear_expired_transients' ), 10 );
 	}
 
 	/**
 	 * Flush the cache
 	 */
+	/*
 	public static function flush_get_job_listings_cache( $post_id ) {
 		if ( 'job_listing' === get_post_type( $post_id ) ) {
 			self::get_transient_version( 'get_job_listings', true );
 		}
 	}
+	*/
 
 	/**
-	 * Flush the cache
+	 * When any post has a term set
 	 */
-	public static function job_manager_my_job_do_action( $action ) {
-		if ( 'mark_filled' === $action || 'mark_not_filled' === $action ) {
-			self::get_transient_version( 'get_job_listings', true );
-		}
+	public static function set_term( $object_id = '', $terms = '', $tt_ids = '', $taxonomy = '' ) {
+		self::get_transient_version( 'gmw_get_' . sanitize_text_field( $taxonomy ), true );
+	}
+
+	/**
+	 * When any term is edited
+	 */
+	public static function edited_term( $term_id = '', $tt_id = '', $taxonomy = '' ) {
+		self::get_transient_version( 'gmw_get_' . sanitize_text_field( $taxonomy ), true );
 	}
 
 	/**
@@ -51,13 +61,26 @@ class GEO_my_WP_Cache_Helper {
 		$transient_name  = $group . '-transient-version';
 		$transient_value = get_transient( $transient_name );
 
-		if ( false === $transient_value || true === $refresh ) {
-			$transient_value = time();
-			set_transient( $transient_name, $transient_value );
-		}
+		//echo $transient_name;
+		//echo $transient_value;
 
-		echo $transient_value;
+		if ( false === $transient_value || true === $refresh ) {
+			self::delete_version_transients( $transient_value );
+			set_transient( $transient_name, $transient_value = time() );
+		}
 		return $transient_value;
+	}
+
+	/**
+	 * When the transient version increases, this is used to remove all past transients to avoid filling the DB.
+	 *
+	 * Note; this only works on transients appended with the transient version, and when object caching is not being used.
+	 */
+	private static function delete_version_transients( $version ) {
+		if ( ! wp_using_ext_object_cache() && ! empty( $version ) ) {
+			global $wpdb;
+			$wpdb->query( $wpdb->prepare( "DELETE FROM {$wpdb->options} WHERE option_name LIKE %s;", "\_transient\_%" . $version ) );
+		}
 	}
 
     /**
@@ -66,41 +89,15 @@ class GEO_my_WP_Cache_Helper {
 	public static function clear_expired_transients() {
 		global $wpdb;
 
-		if ( ! defined( 'WP_SETUP_CONFIG' ) && ! defined( 'WP_INSTALLING' ) ) {
-			$rows = $wpdb->query( "
-				DELETE
-					a, b
-				FROM
-					{$wpdb->options} a, {$wpdb->options} b
-				WHERE
-					a.option_name LIKE '\_transient\_jm\_%' AND
-					a.option_name NOT LIKE '\_transient\_timeout\_jm\_%' AND
-					b.option_name = CONCAT(
-						'_transient_timeout_jm_',
-						SUBSTRING(
-							a.option_name,
-							CHAR_LENGTH('_transient_jm_') + 1
-						)
-					)
-					AND b.option_value < UNIX_TIMESTAMP()
-			" );
-			$rows2 = $wpdb->query( "
-				DELETE
-					a, b
-				FROM
-					{$wpdb->options} a, {$wpdb->options} b
-				WHERE
-					a.option_name LIKE '\_site\_transient\_jm\_%' AND
-					a.option_name NOT LIKE '\_site\_transient\_timeout\_jm\_%' AND
-					b.option_name = CONCAT(
-						'_site_transient_timeout_jm_',
-						SUBSTRING(
-							a.option_name,
-							CHAR_LENGTH('_site_transient_jm_') + 1
-						)
-					)
-					AND b.option_value < UNIX_TIMESTAMP()
-			" );
+		if ( ! wp_using_ext_object_cache() && ! defined( 'WP_SETUP_CONFIG' ) && ! defined( 'WP_INSTALLING' ) ) {
+			echo 'transient deleted';
+			sdf();
+			$sql = "DELETE a, b FROM $wpdb->options a, $wpdb->options b
+				WHERE a.option_name LIKE %s
+				AND a.option_name NOT LIKE %s
+				AND b.option_name = CONCAT( '_transient_timeout_', SUBSTRING( a.option_name, 12 ) )
+				AND b.option_value < %d";
+			$rows = $wpdb->query( $wpdb->prepare( $sql, $wpdb->esc_like( '_transient_gmw_' ) . '%', $wpdb->esc_like( '_transient_timeout_gmw_' ) . '%', time() ) );
 		}
 	}
 }
