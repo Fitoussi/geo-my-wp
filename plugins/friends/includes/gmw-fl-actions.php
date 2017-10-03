ad69f46dfbc051da33779f462f7df0cff4489890
Eyal Fitoussi <info@geomywp.com>
2017-10-02 23:08:34 -1000

    Code cleanup

--- plugins/friends/includes/gmw-fl-actions.php
+++ plugins/friends/includes/gmw-fl-actions.php
@@ -1,10 +1,12 @@
 <?php
 function gmw_fl_activity_actions() {
+	
 	global $bp;
 
 	// Bail if activity is not active
-	if ( ! bp_is_active( 'activity' ) )
+	if ( ! bp_is_active( 'activity' ) ) {
 		return false;
+	}
 
 	bp_activity_set_action( $bp->gmw_location->id, 'gmw_location', __( "Member's location updated", 'GMW' ) );
 	
