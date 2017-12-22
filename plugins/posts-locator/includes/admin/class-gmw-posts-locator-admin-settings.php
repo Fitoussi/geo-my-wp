80e4155ba0e50bb152493ac94aef35b01b9c9f83
Eyal Fitoussi <info@geomywp.com>
2017-12-21 22:19:11 -1000

    Update admin settings

--- plugins/posts-locator/includes/admin/class-gmw-posts-locator-admin-settings.php
+++ plugins/posts-locator/includes/admin/class-gmw-posts-locator-admin-settings.php
@@ -23,7 +23,6 @@ class GMW_Posts_Locator_Admin_Settings {
         add_filter( 'gmw_admin_settings_setup_defaults', array( $this, 'setup_defaults' ) );
         add_filter( 'gmw_admin_settings_groups', array( $this, 'admin_settings_group' ), 5 );
         add_filter( 'gmw_admin_settings', array( $this, 'admin_settings' ), 5 );
-        add_action( 'gmw_main_settings_edit_post_map_settings', array( $this, 'edit_post_map_settings' ), 10, 5 );
     }
 
     /**
@@ -43,15 +42,11 @@ class GMW_Posts_Locator_Admin_Settings {
                 'contact'    => 1,
                 'days-hours' => 1
             ),
-            'edit_post_map_settings' => array(
-                'latitude'    => '40.711544',
-                'longitude'   => '-74.013486', 
-                'map_type'    => 'ROADMAP',
-                'zoom_level'  => 7   
-            ),         
-            'post_types' => array(
-                'post'
-            )
+            'edit_post_page_map_latitude'   => '40.711544',
+            'edit_post_page_map_longitude'  => '-74.013486', 
+            'edit_post_page_map_type'       => 'ROADMAP',
+            'edit_post_page_map_zoom_level' => 7,       
+            'post_types' => array( 'post' )
         );
 
         return $defaults;
@@ -83,116 +78,92 @@ class GMW_Posts_Locator_Admin_Settings {
      */
     public function admin_settings( $settings ) {
 
-    	$settings['post_types_settings'] = array(
-			'post_types' => array(
-				'name'          => 'post_types',
-                'type'          => 'multiselect',
-				'default'       => '',
-				'label'         => __( 'Post Types', 'GMW' ),
-				'desc'          => __( 'Select the post type where you would like to enable geotagging. GEO my WP Location section will be added to the "Edit Post" page of the selected post types.', 'GMW' ),
-                'options'       => gmw_get_post_types_array(),
-                'attributes'    => array(),
-                'priority'      => 10
-			),
-            'edit_post_map_settings' => array(
-                'name'       => 'edit_post_map_settings',
-                'type'       => 'function',
-                'default'    => array(
-                    'latitude'    => '40.711544',
-                    'longitude'   => '-74.013486', 
-                    'map_type'    => 'ROADMAP',
-                    'zoom_level'  => 7   
+    	$settings['post_types_settings']['post_types'] = array(
+			'name'          => 'post_types',
+            'type'          => 'multiselect',
+			'default'       => '',
+			'label'         => __( 'Post Types', 'GMW' ),
+			'desc'          => __( 'Select the post types where you would like to enable geotagging. GEO my WP Location section will be added to the "Edit Post" page of the selected post types.', 'GMW' ),
+            'options'       => gmw_get_post_types_array(),
+            'attributes'    => array(),
+            'priority'      => 10
+		);
+
+        $zoom_levels = array();
+        
+        for ( $i = 1; $i <= 21; $i++ ) {
+            $zoom_levels[$i] = $i;
+        }
+
+        $settings['post_types_settings']['edit_post_page_options'] = array(
+            'name'          => 'edit_post_page_options',
+            'type'          => 'fields_group',
+            'label'         => __( 'Map Settings ( "Edit Post" Page )', 'GMW' ),
+            'desc'          => __( 'Setup the map of the Location section in the "Edit Post" page.' , 'GMW' ),
+            'fields'        => array(
+                'edit_post_page_map_latitude'  => array(
+                    'name'          => 'edit_post_page_map_latitude',
+                    'type'          => 'text',
+                    'default'       => '40.711544',
+                    'placeholder'   => __( 'Latitude', 'GMW' ),
+                    'label'         => __( 'Default latitude', 'GMW' ),
+                    'desc'          => __( 'Enter the latitude of the default location that will show when the map first loads.', 'GMW' ),
+                    'attributes'    => array(),
+                    'priority'      => 5
                 ),
-                'label'      => __( 'Map Settings ( admin\'s "Edit Post" Page )', 'GMW' ),
-                'desc'       => __( 'Default settings for the map displayed in the Location section in the "Edit Post" page.' , 'GMW' ),
-                'attributes' => array(),
-                'priority'   => 20
+                'edit_post_page_map_longitude'  => array(
+                    'name'          => 'edit_post_page_map_longitude',
+                    'type'          => 'text',
+                    'default'       => '-74.013486',
+                    'placeholder'   => __( 'Longitude', 'GMW' ),
+                    'label'         => __( 'Default longitude', 'GMW' ),
+                    'desc'          => __( 'Enter the longitude of the default location that will show when the map first loads.', 'GMW' ),
+                    'attributes'    => array(),
+                    'priority'      => 10
+                ),
+                'edit_post_page_map_type'   => array(
+                    'name'          => 'edit_post_page_map_type',
+                    'type'          => 'select',
+                    'default'       => 'ROADMAP',
+                    'label'         => __( 'Map type', 'GMW' ),
+                    'desc'          => __( 'Select the map type.', 'GMW' ),
+                    'options'       => array(
+                        'ROADMAP'       => __( 'ROADMAP', 'GMW' ),
+                        'SATELLITE'     => __( 'SATELLITE', 'GMW' ),
+                        'HYBRID'        => __( 'HYBRID', 'GMW' ),
+                        'TERRAIN'       => __( 'TERRAIN', 'GMW' )
+                    ),
+                    'attributes'    => array(),
+                    'priority'      => 15
+                ),
+                'edit_post_page_map_zoom_level'   => array(
+                    'name'          => 'edit_post_page_map_zoom_level',
+                    'type'          => 'select',
+                    'default'       => 7,
+                    'label'         => __( 'Map type', 'GMW' ),
+                    'desc'          => __( 'Select the zoom level of the map.', 'GMW' ),
+                    'options'       => $zoom_levels,
+                    'attributes'    => array(),
+                    'priority'      => 20
+                ),
             ),
-			'mandatory_address' => array(
-				'name'          => 'mandatory_address',
-                'type'          => 'checkbox',
-				'default'       => 0,
-				'label'         => __( 'Mandatory Address fields', 'GMW' ),
-				'cb_label'      => __( 'Yes', 'GMW' ),
-				'desc'          => __( 'Check this checkbox to make the post types address field required.', 'GMW' ),
-				'attributes'    => array(),
-                'priority'      => 30
-			)
-    	);
+            'attributes' => '',
+            'optionsbox' => 1,  
+            'priority'   => 20
+        );
+
+        $settings['post_types_settings']['mandatory_address'] = array(
+            'name'          => 'mandatory_address',
+            'type'          => 'checkbox',
+            'default'       => 0,
+            'label'         => __( 'Mandatory Address fields', 'GMW' ),
+            'cb_label'      => __( 'Yes', 'GMW' ),
+            'desc'          => __( 'Enable this feature to make the post location required.', 'GMW' ),
+            'attributes'    => array(),
+            'priority'      => 30
+        );
 
       	return $settings;
-    }
-
-    /**
-     * Edit post page settings
-     * 
-     * @param  [type] $value        [description]
-     * @param  [type] $name_attr    [description]
-     * @param  [type] $gmw_settings [description]
-     * @param  [type] $key          [description]
-     * @param  [type] $option       [description]
-     * @return [type]               [description]
-     */
-    public function edit_post_map_settings( $value, $name_attr, $gmw_settings, $key, $option ) {
-        
-        $name_attr = esc_attr( $name_attr );
-
-        if ( empty( $value ) ) {
-            $value = $option['default'];
-        }
-        ?>
-        <div class="gmw-options-box">
-            
-            <div class="single-option">
-                
-                <label><?php _e( 'Default Latitude', 'GMW' ); ?>:</label>
-                
-                <div class="option-content">
-                    <input 
-                        type="text" 
-                        class="regular-text text" 
-                        name="<?php echo $name_attr.'[latitude]'; ?>]" 
-                        value="<?php echo esc_attr( $value['latitude'] ); ?>" 
-                        placeholder="Latitude"
-                    />
-                </div>
-                  
-                <label><?php _e( 'Default Latitude', 'GMW' ); ?>:</label>
-                
-                <div class="option-content">
-                    <input 
-                        type="text" 
-                        class="regular-text text" 
-                        name="<?php echo $name_attr.'[longitude]'; ?>]" 
-                        value="<?php echo esc_attr( $value['longitude'] ); ?>" 
-                        placeholder="Longitude"
-                    />
-                </div>
-            
-                <label><?php _e( 'Map Type', 'GMW' ); ?>:</label>
-
-                <div class="option-content">
-                    <select name="<?php echo $name_attr.'[map_type]'; ?>">
-                        <?php foreach ( array( 'ROADMAP', 'SATELLITE', 'HYBRID', 'TERRAIN' ) as $option ) { ?>
-                            <option value="<?php echo $option; ?>" <?php if ( $option == $value['map_type'] ) echo 'selected="selected"'; ?>><?php echo $option; ?></option> 
-                        <?php } ?>                                                             
-                    </select>
-                </div>
-         
-                <label><?php _e( 'Zoom Level', 'GMW' ); ?>:</label>
-                
-                <div class="option-content">
-                    <select name="<?php echo $name_attr.'[zoom_level]'; ?>">
-                        <?php for ( $i = 1; $i <= 21; $i++ ) { ?>
-                            <option value="<?php echo $i; ?>" <?php if ( $i == $value['zoom_level'] ) echo 'selected="selected"'; ?> >
-                                <?php echo $i; ?>
-                            </option>
-                        <?php } ?>    
-                    </select>
-                </div>
-            </div>
-        </div>
-        <?php
     }
 }
 new GMW_Posts_Locator_Admin_Settings();
