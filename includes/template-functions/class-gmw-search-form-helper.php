f8eb6de35b3b5f4d94a2f19f679e91ea9bb29508
Eyal Fitoussi <info@geomywp.com>
2018-02-28 23:49:47 -1000

    Modify options_builder function.
    - add args filter.
    - allow to pass options via $args. can still pass via it’s own argument.
    - New function : bp_member_types_filter()
    - New function : bp_groups_filter()

--- includes/template-functions/class-gmw-search-form-helper.php
+++ includes/template-functions/class-gmw-search-form-helper.php
@@ -29,11 +29,20 @@ class GMW_Search_Form_Helper {
             'usage'            => 'dropdown',
             'object'       	   => '',
             'show_options_all' => '',
+            'options'		   => array(),
             'name_tag'         => ''
         );
 
+		// can pass $options via args or separate array as argument
+		if ( ! empty( $options ) ) {
+			$args['options'] = $options;
+		}
+
         $args = wp_parse_args( $args, $defaults );
+
+        // modify the args
         $args = apply_filters( 'gmw_search_form_options_selector_builder_args', $args );
+        $args = apply_filters( 'gmw_search_form_'.str_replace( '-', '_', $args['object'] ).'_options_selector_builder_args', $args );
 
         $id_tag = $args['id_tag'] != '' ? 'id="'.$args['id_tag'].'"' : '';     
 
@@ -41,7 +50,7 @@ class GMW_Search_Form_Helper {
 
         if ( $args['usage'] == 'hidden' ) {
 
-	        foreach ( $options as $value => $name ) {
+	        foreach ( $args['options'] as $value => $name ) {
 	        	$output .= '<input type="hidden" name="'.esc_attr( $args['name_tag'] ).'[]" value="'.esc_attr( sanitize_text_field( $value ) ).'" />';
 	        }
 
@@ -54,7 +63,7 @@ class GMW_Search_Form_Helper {
 	            $output .= '<option value="">'.esc_html( $args['show_options_all'] ).'</option>';
 	        }
 
-	        foreach ( $options as $value => $name ) {
+	        foreach ( $args['options'] as $value => $name ) {
 	    	
 	            $selected = ( isset( $_GET[$args['name_tag']] ) && in_array( $value, $_GET[$args['name_tag']] ) ) ? 'selected="selected"' : '';
 
@@ -65,7 +74,7 @@ class GMW_Search_Form_Helper {
 
 	    // add custom styles
 	    } else {
-            $output .= apply_filters( 'gmw_search_form_'.$args['usage'].'_options_selector', $output, $args, $options );
+            $output .= apply_filters( 'gmw_search_form_'.$args['usage'].'_options_selector', $output, $args, $args['options'] );
 	    }
 
 	    return $output;
@@ -233,7 +242,7 @@ class GMW_Search_Form_Helper {
 	        $output .= "</select>";
 
 		} else {
-	        $output = '<input type="hidden" name="'.$url_px.'units" value="'.esc_attr( sanitize_text_field( $args['units'] ) ).'" />';
+	        $output = '<input type="hidden" id="gmw-units-'.$id.'" name="'.$url_px.'units" value="'.esc_attr( sanitize_text_field( $args['units'] ) ).'" />';
 		} 
 		
 		return $output;
@@ -316,28 +325,21 @@ class GMW_Search_Form_Helper {
 	 * @param  array  $args [description]
 	 * @return [type]       [description]
 	 */
-	public static function submission_fields( $id = 0, $per_page = 10 ) {
+	public static function submission_fields( $id = 0, $per_page = '' ) {
 
-		$id 	 = absint( $id );
-		$url_px  = gmw_get_url_prefix();
-		$lat 	 = ! empty( $_GET[$url_px.'lat'] ) ? esc_attr( urldecode( $_GET[$url_px.'lat'] ) ) : '';
-		$lng     = ! empty( $_GET[$url_px.'lng'] ) ? esc_attr( urldecode( $_GET[$url_px.'lng'] ) ) : '';
-		$state   = ! empty( $_GET['state'] ) ? esc_attr( urldecode( $_GET['state'] ) ) : false;
-		$country = ! empty( $_GET['country'] ) ? esc_attr( urldecode( $_GET['country'] ) ) : false;
-
-		$address = '';
-
-		if ( isset( $_GET[$url_px.'address'] ) ) {
-			$address = is_array( $_GET[$url_px.'address'] ) ? implode( ' ', $_GET[$url_px.'address'] ) : $_GET[$url_px.'address'];
-			$address = esc_attr( stripslashes( $address ) );
-		}
+		$id 	  = absint( $id );
+		$per_page = esc_attr( $per_page );
+		$url_px   = gmw_get_url_prefix();
+		$lat 	  = ! empty( $_GET[$url_px.'lat'] ) ? esc_attr( urldecode( $_GET[$url_px.'lat'] ) ) : '';
+		$lng      = ! empty( $_GET[$url_px.'lng'] ) ? esc_attr( urldecode( $_GET[$url_px.'lng'] ) ) : '';
+		$state    = ! empty( $_GET['state'] ) 	   ? esc_attr( urldecode( $_GET['state'] ) ) : false;
+		$country  = ! empty( $_GET['country'] ) 	   ? esc_attr( urldecode( $_GET['country'] ) ) : false;
 
 		// generate fields
 	    $output  = "<div id=\"gmw-submission-fields-{$id}\" class=\"gmw-submission-fields\" data-form_id=\"{$id}\" style=\"display:none\">";
 	    // set the page number to 1. We do this to reset the page number when form submitted again
 	    $output .= "<input type=\"hidden\" id=\"gmw-page-{$id}\" class=\"gmw-page\" name=\"page\" value=\"1\" />";
 	    $output .= "<input type=\"hidden\" id=\"gmw-per-page-{$id}\" class=\"gmw-per-page\" name=\"{$url_px}per_page\" value=\"{$per_page}\" />";   	
-	    $output .= "<input type=\"hidden\" id=\"prev-address-{$id}\" class=\"prev-address\" value=\"{$address}\"/>";
 	    $output .= "<input type=\"hidden\" id=\"gmw-lat-{$id}\" class=\"gmw-lat\" name=\"{$url_px}lat\" value=\"{$lat}\"/>";      	
 	    $output .= "<input type=\"hidden\" id=\"gmw-lng-{$id}\" class=\"gmw-lng\" name=\"{$url_px}lng\" value=\"{$lng}\"/>";
 	    $output .= "<input type=\"hidden\" id=\"gmw-form-id-{$id}\" class=\"gmw-form-id\" name=\"{$url_px}form\" value=\"{$id}\" />";
@@ -347,8 +349,159 @@ class GMW_Search_Form_Helper {
 	    $disabled = $country == false ? 'disabled="disabled"' : '';
 	    $output .= "<input type=\"hidden\" id=\"gmw-country-{$id}\" class=\"gmw-country\" name=\"country\" value=\"{$country}\" {$disabled}/>";
 
+	    $output = apply_filters( 'gmw_submission_fields', $output );
+
 	    $output .= '</div>';
 
 	    return $output;
-	}	
+	}
+
+	/**
+	 * Search form BP Member Types filter
+	 * 
+	 * @param  array  $gmw [description]
+	 * @return [type]      [description]
+	 */
+	public static function bp_member_types_filter( $args = array(), $member_types = array() ) {
+
+		$url_px = gmw_get_url_prefix();
+      	
+        $defaults = array(
+            'id'               => 1,
+            'usage'            => 'smartbox',
+            'id_tag'           => '',
+            'class_tag'        => '',
+            'name_tag'         => $url_px.'bpmt',
+            'object'           => 'bp-member-types',
+            'show_options_all' => __( 'Search member types', 'GMW' )
+        );
+
+        $args = wp_parse_args( $args, $defaults );
+
+        if ( $args['id_tag'] == '' ) {
+            $args['id_tag'] = 'gmw-bp-member-types-'.$args['id'];
+        }
+        	      	        
+        /**
+      	 * If types are not provided
+      	 * 
+      	 * we will get all types rgistered types.
+      	 */
+      	if ( empty( $member_types ) ) {
+	      	
+	      	$member_types = array();
+
+	        foreach( bp_get_member_types( array(), 'object' ) as $type ) {
+	            $member_types[$type->name] = $type->labels['name'];
+	        }
+	    }
+
+	    return GMW_Search_Form_Helper::options_selector_builder( $args, $member_types );
+	}
+
+	/**
+     * BP groups filter
+     *
+     * We place this function here since multiple add-ons will be using it.
+     *
+     * So we do not want to have the functiosn duplicated.
+     * 
+     * @param  array  $args   [description]
+     * @param  array  $groups [description]
+     * @return [type]         [description]
+     */
+    public static function bp_groups_filter( $args = array(), $groups = array() ) {
+
+        if ( ! function_exists( 'bp_is_active' ) || ! bp_is_active( 'groups' ) ) {
+            return;
+        }
+
+        $url_px = gmw_get_url_prefix();
+      
+        $defaults = array(
+            'id'               => 1,
+            'usage'            => 'smartbox',
+            'id_tag'           => '',
+            'class_tag'        => '',
+            'name_tag'         => $url_px.'bp_groups',
+            'object'           => 'bp-groups',
+            'show_options_all' => __( 'Search groups', 'gmw-premium-settings' )
+        );
+
+        $args = wp_parse_args( $args, $defaults );
+
+        if ( $args['id_tag'] == '' ) {
+            $args['id_tag'] = 'gmw-bp-groups-'.$args['id'];
+        }
+        
+        /**
+         * Use BP built in class to have more options when pulling groups from database.
+         * This might be a bit more memory consuming and so it is disabled by default.
+         */
+        if ( apply_filters( 'gmw_ps_advanced_get_bp_groups_list', false ) ) {
+
+            $groups = BP_Groups_Group::get( apply_filters( 'gmw_search_form_get_groups_list_args', array(
+                'type'               => 'alphabetical',
+                'per_page'           => 999,
+                'orderby'            => 'date_created',
+                'order'              => 'DESC',
+                'page'               => null,
+                'user_id'            => 0,
+                'slug'               => array(),
+                'search_terms'       => false,
+                'search_columns'     => array(),
+                'group_type'         => '',
+                'group_type__in'     => '',
+                'group_type__not_in' => '',
+                'meta_query'         => false,
+                'include'            => ! empty( $groups ) ? $groups : '',
+                'parent_id'          => null,
+                'update_meta_cache'  => true,
+                'update_admin_cache' => false,
+                'exclude'            => false,
+                'show_hidden'        => false,
+                'status'             => array()
+            ) ) );
+
+            $groups = $groups['groups'];
+
+        /**
+         * Simpler method to retrive the list of groups
+         */
+        } else {
+
+            global $wpdb;
+
+            $where = '';
+
+            if ( ! empty( $groups ) ) {
+                $groups     = array_map( 'absint', $groups );
+                $groups_var = implode( ',', $groups );
+                $where      = "WHERE id IN ( {$groups_var} )";
+            }
+
+            $groups = $wpdb->get_results( "
+                SELECT id, name 
+                FROM {$wpdb->prefix}bp_groups
+                {$where}
+                "
+            );
+        }
+
+        $options = array();
+
+        foreach( $groups as $group ) {
+            
+            if ( $group->id == 0 ) {
+                continue;
+            }
+
+            $options[$group->id] = $group->name;
+        }
+
+        $args['options'] = $options;
+
+        // get the list of groups
+        return GMW_Search_Form_Helper::options_selector_builder( $args );
+    }
 }
