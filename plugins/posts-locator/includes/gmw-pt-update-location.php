1afb9376fbee0e84ff21ad52c47aac6dbcc46acb
Eyal Fitoussi <info@geomywp.com>
2018-03-17 13:13:09 -1000

    update description text.

--- plugins/posts-locator/includes/gmw-pt-update-location.php
+++ plugins/posts-locator/includes/gmw-pt-update-location.php
@@ -1,7 +1,9 @@
 <?php
 /**
  * 
- * The function gmw_pt_update_location() does not exist here anymore. The function is deprecated and replaced with gmw_update_post_location() which can be
+ * The function gmw_pt_update_location() does not exist here anymore. 
  * 
- * found in geo-my-wp/plugins/posts/includes/gmw-pt-functions.php
+ * The function is deprecated and replaced with gmw_update_post_location() which can be
+ * 
+ * found in geo-my-wp/plugins/posts-locator/includes/gmw-posts-locator-functions.php
  */
