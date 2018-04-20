a97397531171f4573b4dc330e76061e819531229
Eyal Fitoussi <info@geomywp.com>
2018-04-20 00:02:45 -1000

    Code Standards

--- plugins/members-locator/includes/class-gmw-member-location-form.php
+++ plugins/members-locator/includes/class-gmw-member-location-form.php
@@ -1,7 +1,7 @@
 <?php
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 /**
@@ -10,66 +10,67 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Location form for Location tab of BuddyPress member's profile page.
  *
  * @since 3.0
- * 
  */
 class GMW_Member_Location_Form extends GMW_Location_Form {
 
-    /**
-     * addon
-     * @var string
-     */
-    public $slug = 'members_locator';
+	/**
+	 * addon
+	 *
+	 * @var string
+	 */
+	public $slug = 'members_locator';
 
-    /**
-     * [$object_type description]
-     * @var string
-     */
-    public $object_type = 'user'; 
+	/**
+	 * [$object_type description]
+	 *
+	 * @var string
+	 */
+	public $object_type = 'user';
 
-    /**
-     * Run the form class
-     * 
-     * @param array $attr [description]
-     */
-    public function __construct( $attr = array() ) {
+	/**
+	 * Run the form class
+	 *
+	 * @param array $attr [description]
+	 */
+	public function __construct( $attr = array() ) {
 
-        parent::__construct( $attr );
+		parent::__construct( $attr );
 
-        // add custom tab panels
-        add_action( 'gmw_lf_content_end',  array( $this, 'create_tabs_panels' ) ); 
-    }
+		// add custom tab panels
+		add_action( 'gmw_lf_content_end', array( $this, 'create_tabs_panels' ) );
+	}
 
-    /**
-     * Additional custom form tabs
-     *     
-     * @return array
-     */
-    public function form_tabs() {
-        
-        $tabs = parent::form_tabs();
+	/**
+	 * Additional custom form tabs
+	 *
+	 * @return array
+	 */
+	public function form_tabs() {
 
-        return apply_filters( 'gmw_member_location_form_tabs', $tabs, $this->args );
-    }
+		$tabs = parent::form_tabs();
 
-    /**
-     * Additional form fields
-     * 
-     * @return array
-     */
-    public function form_fields() {
+		return apply_filters( 'gmw_member_location_form_tabs', $tabs, $this->args );
+	}
 
-        // retreive parent fields
-        $fields = parent::form_fields();
+	/**
+	 * Additional form fields
+	 *
+	 * @return array
+	 */
+	public function form_fields() {
 
-        return apply_filters( 'gmw_member_location_tab_fields', $fields, $this->args );
-    }
+		// retreive parent fields
+		$fields = parent::form_fields();
 
-    /**
-     * Generate custom tabs panels
-     * 
-     * @return void
-     */
-    public function create_tabs_panels() {
-        do_action( 'gmw_member_location_tabs_panels', $this );
-    }
+		return apply_filters( 'gmw_member_location_tab_fields', $fields, $this->args );
+	}
+
+	/**
+	 * Generate custom tabs panels
+	 *
+	 * @return void
+	 */
+	public function create_tabs_panels() {
+		do_action( 'gmw_member_location_tabs_panels', $this );
+	}
 }
