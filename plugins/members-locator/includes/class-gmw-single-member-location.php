486743e766b0cfc36f1973ee7d3a23c99822d53c
Eyal Fitoussi <info@geomywp.com>
2018-04-20 00:08:11 -1000

    code standards.

--- plugins/members-locator/includes/class-gmw-single-member-location.php
+++ plugins/members-locator/includes/class-gmw-single-member-location.php
@@ -1,7 +1,7 @@
 <?php
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 if ( ! class_exists( 'GMW_Single_Location' ) ) {
@@ -10,32 +10,32 @@ if ( ! class_exists( 'GMW_Single_Location' ) ) {
 
 /**
  * GMW_Single_Member_Location Class
-*
-* Display different location elements of a post.
-*
-* @version 1.0
-* 
-* @author Eyal Fitoussi
-* 
-* @since 2.6.1
-*/
+ *
+ * Display different location elements of a post.
+ *
+ * @version 1.0
+ *
+ * @author Eyal Fitoussi
+ *
+ * @since 2.6.1
+ */
 class GMW_Single_BP_Member_Location extends GMW_Single_Location {
 
 	/**
 	 * Extends the default shortcode atts
+	 *
 	 * @since 2.6.1
 	 * Public $args
-	 *
 	 */
 	protected $args = array(
-		'elements'			  => 'name,address,map,distance,directions_link',
-		'object'	 	  	  => 'member',
-		'prefix'	 		  => 'fl',
-		'item_info_window'	  => 'name,address,distance',
+		'elements'            => 'name,address,map,distance,directions_link',
+		'object'              => 'member',
+		'prefix'              => 'fl',
+		'item_info_window'    => 'name,address,distance',
 		'show_in_single_post' => 1,
-		'no_location_message' => ''
+		'no_location_message' => '',
 	);
-		
+
 	public function __construct( $atts = [] ) {
 
 		$this->args['no_location_message'] = __( 'The member has not added a location yet.', 'geo-my-wp' );
@@ -54,7 +54,7 @@ class GMW_Single_BP_Member_Location extends GMW_Single_Location {
 		if ( is_single() && empty( $this->args['show_in_single_post'] ) ) {
 			$this->args['no_location_message'] = false;
 		}
-	
+
 		// check if passing user ID
 		if ( empty( $this->args['object_id'] ) ) {
 
@@ -67,25 +67,24 @@ class GMW_Single_BP_Member_Location extends GMW_Single_Location {
 
 				$this->args['object_id'] = $bp->displayed_user->id;
 
-			// look form member ID in members loop
+				// look form member ID in members loop
 			} elseif ( ! empty( $members_template->member->ID ) ) {
 
 				$this->args['object_id'] = $members_template->member->ID;
 
-			// if in single post page look for post author
+				// if in single post page look for post author
 			} elseif ( is_single() && ! empty( $this->args['show_in_single_post'] ) ) {
 
 				global $post;
 
 				if ( ! empty( $post->post_author ) ) {
-				
+
 					$this->args['object_id'] = $post->post_author;
-				
+
 				} else {
-				
+
 					return false;
 				}
-
 			} else {
 
 				return  false;
@@ -98,16 +97,16 @@ class GMW_Single_BP_Member_Location extends GMW_Single_Location {
 
 	/**
 	 * Get member's name
-	 * 
+	 *
 	 * @return [type] [description]
 	 */
 	public function name() {
-		return apply_filters( 'gmw_sl_title', '<h3 class="gmw-sl-title member-name gmw-sl-element">'. bp_core_get_userlink( $this->args['object_id'] ) .'</h3>', $this->location_data, $this->args, $this->user_position );
+		return apply_filters( 'gmw_sl_title', '<h3 class="gmw-sl-title member-name gmw-sl-element">' . bp_core_get_userlink( $this->args['object_id'] ) . '</h3>', $this->location_data, $this->args, $this->user_position );
 	}
 
 	/**
 	 * Use title for the name
-	 * 
+	 *
 	 * @return [type] [description]
 	 */
 	public function title() {
@@ -117,13 +116,13 @@ class GMW_Single_BP_Member_Location extends GMW_Single_Location {
 
 /**
  * GMW Single member location shortcode
- * 
+ *
  * @version 2.0
- * 
+ *
  * @author Eyal Fitoussi
  */
 function gmw_single_bp_member_location_shortcode( $atts = array() ) {
-	
+
 	if ( empty( $atts ) ) {
 		$atts = [];
 	}
