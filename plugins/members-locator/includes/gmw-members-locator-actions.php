5334af5c31593b8c985c256b8b1afff091a6551f
Eyal Fitoussi <info@geomywp.com>
2018-04-20 00:09:29 -1000

    code standards.

--- plugins/members-locator/includes/gmw-members-locator-actions.php
+++ plugins/members-locator/includes/gmw-members-locator-actions.php
@@ -1,79 +1,79 @@
 <?php
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 /**
  * Filter activity by location
- * 
+ *
  * @param  [type] $where [description]
  * @param  [type] $args  [description]
  * @return [type]        [description]
  */
 function gmw_fl_filter_location_activity( $where, $args ) {
-    
-    if ( isset( $_COOKIE['bp-activity-filter'] ) && $_COOKIE['bp-activity-filter'] == 'gmw_member_location_updated' ) {
-        
-        if ( ! isset( $where['filter_sql'] ) ) {
-        
-            $where['filter_sql'] = " a.type IN ( 'gmw_member_location_updated' )";
-        
-        } else {
-            $where['filter_sql'] .= " AND a.type IN ( 'gmw_member_location_updated' )";
-        }
-    }
-    
-    return $where;
+
+	if ( isset( $_COOKIE['bp-activity-filter'] ) && $_COOKIE['bp-activity-filter'] == 'gmw_member_location_updated' ) {
+
+		if ( ! isset( $where['filter_sql'] ) ) {
+
+			$where['filter_sql'] = " a.type IN ( 'gmw_member_location_updated' )";
+
+		} else {
+			$where['filter_sql'] .= " AND a.type IN ( 'gmw_member_location_updated' )";
+		}
+	}
+
+	return $where;
 }
 add_filter( 'bp_activity_get_where_conditions', 'gmw_fl_filter_location_activity', 50, 2 );
 
 /**
  * Get the member name to save as location title before location is saved
- * 
+ *
  * @param  [type] $location [description]
  * @return [type]           [description]
  */
 function gmw_fl_get_member_name( $location ) {
 
-    $name = bp_core_get_username( $location['object_id'] );
+	$name = bp_core_get_username( $location['object_id'] );
 
-    if ( ! empty( $name ) ) {
-        $location['title'] = sanitize_text_field( stripslashes( $name ) );
-    }
-    
-    return $location;
+	if ( ! empty( $name ) ) {
+		$location['title'] = sanitize_text_field( stripslashes( $name ) );
+	}
+
+	return $location;
 }
 add_filter( 'gmw_lf_user_location_args_before_location_updated', 'gmw_fl_get_member_name' );
 
 /**
  * Add location item to dropdown menu filter
- * 
+ *
  * @return [type] [description]
  */
 function gmw_fl_location_filter_options() {
 ?>
-    <option value="gmw_member_location_updated"><?php _e( 'Member Location', 'geo-my-wp' ); ?></option>
+	<option value="gmw_member_location_updated"><?php _e( 'Member Location', 'geo-my-wp' ); ?></option>
 <?php
 }
-add_action( 'bp_activity_filter_options',        'gmw_fl_location_filter_options', 10 );
+add_action( 'bp_activity_filter_options', 'gmw_fl_location_filter_options', 10 );
 add_action( 'bp_member_activity_filter_options', 'gmw_fl_location_filter_options', 10 );
 
 /**
  * Register location activity for members
- * 
+ *
  * @return [type] [description]
  */
 function gmw_members_locator_activity_actions() {
-    
-    // abort if activity is not active
-    if ( ! bp_is_active( 'activity' ) ) {
-        return false;
-    }
 
-    bp_activity_set_action( buddypress()->members->id, 'gmw_member_location_updated', __( 'Member location updated', 'geo-my-wp' ) );
-    
-    do_action( 'gmw_fl_activity_actions' );
+	// abort if activity is not active
+	if ( ! bp_is_active( 'activity' ) ) {
+		return false;
+	}
+
+	bp_activity_set_action( buddypress()->members->id, 'gmw_member_location_updated', __( 'Member location updated', 'geo-my-wp' ) );
+
+	do_action( 'gmw_fl_activity_actions' );
 }
 add_action( 'bp_register_activity_actions', 'gmw_members_locator_activity_actions' );
 
@@ -81,11 +81,11 @@ add_action( 'bp_register_activity_actions', 'gmw_members_locator_activity_action
  * Record member activity after location updated
  *
  * @since 3.0
- * 
+ *
  * @param  object $location   updated location
- * @param  array $form_values location form values
- * 
- * @return mixed              
+ * @param  array  $form_values location form values
+ *
+ * @return mixed
  */
 function gmw_after_member_location_updated( $user_location, $form_values ) {
 
@@ -94,31 +94,31 @@ function gmw_after_member_location_updated( $user_location, $form_values ) {
 		return;
 	}
 
-    // verify User ID
-    if ( empty( $form_values['gmw_location_form']['object_id'] ) ) {
-        
-        trigger_error( 'Invalid user ID.', E_USER_NOTICE );
+	// verify User ID
+	if ( empty( $form_values['gmw_location_form']['object_id'] ) ) {
 
-        return;
+		trigger_error( 'Invalid user ID.', E_USER_NOTICE );
 
-    } else {
+		return;
 
-        $user_id = $form_values['gmw_location_form']['object_id'];
-    }
+	} else {
 
-    // hook from previous versions of GEO my WP
-    // do something after location updated
-    do_action( 'gmw_fl_after_location_saved', $user_id , $user_location, $form_values );
+		$user_id = $form_values['gmw_location_form']['object_id'];
+	}
 
-    // filter allows to disable activity update
-    if ( ! apply_filters( 'gmw_fl_disable_location_activity_update', false ) ) {
+	// hook from previous versions of GEO my WP
+	// do something after location updated
+	do_action( 'gmw_fl_after_location_saved', $user_id, $user_location, $form_values );
 
-        include_once( 'gmw-members-locator-activity.php' );
+	// filter allows to disable activity update
+	if ( ! apply_filters( 'gmw_fl_disable_location_activity_update', false ) ) {
 
-        // update activity
-        if ( function_exists( 'gmw_record_member_location_activity' ) ) {
-            gmw_record_member_location_activity( $user_id, $user_location );
-        }
-    }
+		include_once( 'gmw-members-locator-activity.php' );
+
+		// update activity
+		if ( function_exists( 'gmw_record_member_location_activity' ) ) {
+			gmw_record_member_location_activity( $user_id, $user_location );
+		}
+	}
 }
-add_action( 'gmw_lf_after_user_location_updated', 'gmw_after_member_location_updated', 10 , 2 );
+add_action( 'gmw_lf_after_user_location_updated', 'gmw_after_member_location_updated', 10, 2 );
