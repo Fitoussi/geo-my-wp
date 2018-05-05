5e2c14df2e2829a0d714e4f38e77136e6623a8b6
Eyal Fitoussi <info@geomywp.com>
2018-05-04 16:21:20 -1000

    Rename $fieldName to $field_name

--- includes/location-form/templates/form-fields/button-field.php
+++ includes/location-form/templates/form-fields/button-field.php
@@ -1,1 +1,1 @@
-<input type="button" class="<?php echo esc_attr( $field['class'] ); ?>" id="<?php echo esc_attr( $field['id'] ); ?>" name="<?php echo esc_attr( $fieldName ); ?>" value="<?php echo esc_attr( $field['label'] ); ?>" />
+<input type="button" class="<?php echo esc_attr( $field['class'] ); ?>" id="<?php echo esc_attr( $field['id'] ); ?>" name="<?php echo esc_attr( $field_name ); ?>" value="<?php echo esc_attr( $field['label'] ); ?>" />
