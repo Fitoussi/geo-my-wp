91536a7c254dc027b5e4ff9fb3fd5dc42b46f1cc
Eyal Fitoussi <info@geomywp.com>
2018-06-16 21:21:44 -0400

    Enhancement: make directions function extendable for different providers.

--- assets/js/gmw.directions.js
+++ assets/js/gmw.directions.js
@@ -1,90 +1,135 @@
-function gmwCalculateRoute( elementId ) {
+/**
+ * Directions Providers.
+ * 
+ * @type {Object}
+ */
+var GMW_Directions_Providers = {
 
+	'google_maps' : function( elementId ) {
+
+		// initialize new service.
+		if ( typeof ( GMW_Directions[elementId] ) === 'undefined' ) {
+			
+			GMW_Directions[elementId] = { 
+				id 		: elementId,
+				service : new google.maps.DirectionsService(),
+				display : new google.maps.DirectionsRenderer( { suppressMarkers: false } )
+			};
+		}
+		
+		var directions = GMW_Directions[elementId],
+		    gmwMap;
+		
+		// look for map with the same object ID as the directions form.
+		if ( typeof( GMW_Maps[elementId] ) !== 'undefined' ) {
+			
+			gmwMap = GMW_Maps[elementId];
+
+			// Clear existing directions.
+			if ( gmwMap.directions != null ) {
+				gmwMap.directions.setMap( null );
+				gmwMap.directions = null;
+			}
+
+			gmwMap.directions = directions.display;	
+			gmwMap.directions.setMap( gmwMap.map );
+		}
+
+		// set direction panel
+		directions.display.setPanel( document.getElementById( 'gmw-directions-panel-wrapper-' + elementId ) );
+		
+	  	var start 	      = jQuery( '#origin-field-' + elementId ).val();
+	  	var end 	      = jQuery( '#destination-field-' + elementId ).val();
+		var travelMode    = jQuery( '#travel-mode-options-' + elementId +' li a.active' ).attr( 'id' );
+		var unitsSystem   = jQuery( '#get-directions-form-' + elementId +' .unit-system-trigger:checked' ).val();
+		var avoidHighways = ( jQuery( '#route-avoid-highways-trigger-' + elementId ).is( ':checked' ) ) ? true : false;
+		var avoidTolls 	  = ( jQuery( '#route-avoid-tolls-trigger-' + elementId ).is( ':checked' ) ) ? true : false;
+		var request 	  = {
+			origin 					 : start,
+			destination 			 : end,
+			travelMode  			 : google.maps.TravelMode[travelMode],
+			unitSystem  			 : google.maps.UnitSystem[unitsSystem],
+			provideRouteAlternatives : true,
+			avoidHighways 		     : avoidHighways,
+			avoidTolls 				 : avoidTolls
+		};
+
+	  	directions.service.route( request, function( response, status ) {
+
+			if ( status == google.maps.DirectionsStatus.OK ) {
+
+		  		directions.display.setDirections( response );	      					      		
+			
+			} else {
+
+				var errorMessage;
+
+		      	// alert an error errorMessage when the route could nog be calculated.
+		      	if ( status == 'ZERO_RESULTS' ) {
+
+		    	  	errorMessage = 'No route could be found between the origin and destination.';
+		      	
+		      	} else if (status == 'UNKNOWN_ERROR') {
+		    	  
+		    	  	errorMessage = 'A directions request could not be processed due to a server error. The request may succeed if you try again.';
+		      
+		      	} else if (status == 'REQUEST_DENIED') {
+		    	  	
+		    	  	errorMessage = 'This webpage is not allowed to use the directions service.';
+		      	
+		      	} else if (status == 'OVER_QUERY_LIMIT') {
+		    		
+		    		errorMessage = 'The webpage has gone over the requests limit in too short a period of time.';
+		      
+		      	} else if (status == 'NOT_FOUND') {
+		    	  
+		    	  	errorMessage = 'At least one of the origin, destination, or waypoints could not be geocoded.';
+		      
+		      	} else if (status == 'INVALID_REQUEST') {
+		    	  
+		    	  	errorMessage = 'The DirectionsRequest provided was invalid.';         
+		      	
+		      	} else {
+		    	  
+		    	  	errorMessage = "There was an unknown error in your request. Requeststatus: nn" + status;
+		      	}
+
+		      	jQuery( '#gmw-directions-panel-wrapper-' + elementId ).html( '<div id="error-message">' + errorMessage + '</div>' );
+			}
+			
+			jQuery( '#get-directions-submit-' + elementId ).removeClass( 'gmw-icon-spin-light animate-spin' ).addClass( 'gmw-icon-search' );
+	  	});
+	}
+}
+
+/**
+ * Directions objects.
+ * 
+ * @type {Object}
+ */
+var GMW_Directions = {};
+
+/**
+ * Get directions function.
+ * 
+ * @param  {[type]} elementId [description]
+ * @return {[type]}           [description]
+ */
+function gmwGetDirections( elementId, provider ) {
+
+	provider = provider || gmwVars.mapsProvider;
+
+	// Loading icon.
 	jQuery( '#get-directions-submit-' + elementId ).removeClass( 'gmw-icon-search' ).addClass( 'gmw-icon-spin-light animate-spin' );
 
 	// clear directions panel
 	jQuery( '#gmw-directions-panel-wrapper-' + elementId ).html( '' );
 
-	// clear existing directions
-	if ( typeof directionsDisplay !== 'undefined' ) {
-		directionsDisplay.setMap( null );
+	if ( typeof( GMW_Directions_Providers[provider] ) === 'undefined' ) {
+		return console.log( provider + ' directions provider not found' );
 	}
 
-	// init new directionsDisplay;
-	directionsDisplay = new google.maps.DirectionsRenderer( { suppressMarkers: false } );		
-	directionsService = new google.maps.DirectionsService();
-
-	// look for map with the same object ID as the directions form
-	// if there is a map we can show the directions on it.
-	if ( GMW_Maps[elementId].data != undefined ) {
-		directionsDisplay.setMap( GMW_Maps[elementId].data.map );
-	}
-	
-	// set direction panel
-	directionsDisplay.setPanel( document.getElementById( 'gmw-directions-panel-wrapper-' + elementId ) );
-	
-  	var start 	      = jQuery( '#origin-field-' + elementId ).val();
-  	var end 	      = jQuery( '#destination-field-' + elementId ).val();
-	var travelMode    = jQuery( '#travel-mode-options-' + elementId +' li a.active' ).attr( 'id' );
-	var unitsSystem   = jQuery( '#get-directions-form-' + elementId +' .unit-system-trigger:checked' ).val();
-	var avoidHighways = ( jQuery( '#route-avoid-highways-trigger-' + elementId ).is( ':checked' ) ) ? true : false;
-	var avoidTolls 	  = ( jQuery( '#route-avoid-tolls-trigger-' + elementId ).is( ':checked' ) ) ? true : false;
-	
-	var request = {
-		origin 					 : start,
-		destination 			 : end,
-		travelMode  			 : google.maps.TravelMode[travelMode],
-		unitSystem  			 : google.maps.UnitSystem[unitsSystem],
-		provideRouteAlternatives :  true,
-		avoidHighways 		     : avoidHighways,
-		avoidTolls 				 : avoidTolls
-	};
-
-  	directionsService.route( request, function( response, status ) {
-
-		if ( status == google.maps.DirectionsStatus.OK ) {
-
-	  		directionsDisplay.setDirections( response );	      					      		
-		
-		} else {
-
-			var errorMessage;
-
-	      	// alert an error errorMessage when the route could nog be calculated.
-	      	if ( status == 'ZERO_RESULTS' ) {
-
-	    	  	errorMessage = 'No route could be found between the origin and destination.';
-	      	
-	      	} else if (status == 'UNKNOWN_ERROR') {
-	    	  
-	    	  	errorMessage = 'A directions request could not be processed due to a server error. The request may succeed if you try again.';
-	      
-	      	} else if (status == 'REQUEST_DENIED') {
-	    	  	
-	    	  	errorMessage = 'This webpage is not allowed to use the directions service.';
-	      	
-	      	} else if (status == 'OVER_QUERY_LIMIT') {
-	    		
-	    		errorMessage = 'The webpage has gone over the requests limit in too short a period of time.';
-	      
-	      	} else if (status == 'NOT_FOUND') {
-	    	  
-	    	  	errorMessage = 'At least one of the origin, destination, or waypoints could not be geocoded.';
-	      
-	      	} else if (status == 'INVALID_REQUEST') {
-	    	  
-	    	  	errorMessage = 'The DirectionsRequest provided was invalid.';         
-	      	
-	      	} else {
-	    	  
-	    	  	errorMessage = "There was an unknown error in your request. Requeststatus: nn" + status;
-	      	}
-
-	      	jQuery('#gmw-directions-panel-wrapper-' + elementId).html( '<div id="error-message">' + errorMessage + '</div>' );
-		}
-		
-		jQuery( '#get-directions-submit-' + elementId ).removeClass( 'gmw-icon-spin-light animate-spin' ).addClass( 'gmw-icon-search' );
-  	});
+	GMW_Directions_Providers[provider]( elementId );
 }
 
 jQuery( document ).ready(function($) {
@@ -98,7 +143,7 @@ jQuery( document ).ready(function($) {
 		elementId = $( this ).closest( 'form' ).find( '.element-id' ).val();
 
 		// run the directions functions
-		gmwCalculateRoute( elementId  );
+		gmwGetDirections( elementId  );
 	});
 	
 	// if press enter in address field.
@@ -114,7 +159,7 @@ jQuery( document ).ready(function($) {
 			elementId = $( this ).closest( 'form' ).find( '.element-id' ).val();
 
 			// run the directions functions
-			gmwCalculateRoute( elementId );
+			gmwGetDirections( elementId );
 	    }
 	});
 
@@ -128,7 +173,7 @@ jQuery( document ).ready(function($) {
 
 		elementId = $( this ).closest( 'form' ).find( '.element-id' ).val();
 
-		gmwCalculateRoute( elementId  );
+		gmwGetDirections( elementId  );
 	});
 
 	$( document ).on( 'change', '.gmw-directions-form-wrapper .unit-system-options .unit-system-trigger', function(e) {
@@ -139,7 +184,7 @@ jQuery( document ).ready(function($) {
 		
 		$( this ).closest( 'label' ).addClass( 'active' );
 
-		gmwCalculateRoute( elementId  );
+		gmwGetDirections( elementId  );
 	});	
 
 	$( document ).on( 'click', '.gmw-iw-close-button', function(e) {	
@@ -154,6 +199,6 @@ jQuery( document ).ready(function($) {
 		
 		elementId = $(this).closest('form').find('.element-id').val();
 		
-		gmwCalculateRoute( elementId  );
+		gmwGetDirections( elementId  );
 	});
 });
