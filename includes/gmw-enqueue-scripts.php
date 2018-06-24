1bc87f183bb2155f52853c6f030e4a4dd08612e7
Eyal Fitoussi <info@geomywp.com>
2018-06-25 00:22:15 +0300

    Update enqueue scripts file.
    - New function to register Google Maps API key.
    - Register LeafLet when needed.
    - update new core and bundled JS and CSS files.
    - update localized JS variables.

--- includes/gmw-enqueue-scripts.php
+++ includes/gmw-enqueue-scripts.php
@@ -5,6 +5,45 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * Register Google Maps API.
+ *
+ * @return [type] [description]
+ *
+ * @since 3.1
+ */
+function gmw_register_google_maps_api() {
+
+	if ( apply_filters( 'gmw_google_maps_api', true ) ) {
+
+		if ( ! wp_script_is( 'google-maps', 'registered' ) ) {
+
+			$china = gmw_get_option( 'api_providers', 'google_maps_api_china', '' );
+			$url   = empty( $china ) ? '://maps.googleapis.com/maps/api/js?' : '://maps.google.cn/maps/api/js?';
+
+			// Generate Google API url. elements can be modified via filters
+			$google_url = apply_filters(
+				'gmw_google_maps_api_url', array(
+					'protocol' => 'https',
+					'url_base' => $url,
+					'url_data' => http_build_query(
+						apply_filters(
+							'gmw_google_maps_api_args', array(
+								'libraries' => 'google_maps' == GMW()->maps_provider ? 'places' : '',
+								'key'       => gmw_get_option( 'api_providers', 'google_maps_server_api_key', '' ),
+								'region'    => gmw_get_option( 'general_settings', 'country_code', 'us' ),
+								'language'  => gmw_get_option( 'general_settings', 'language_code', 'en' ),
+							)
+						), '', '&amp;'
+					),
+				), gmw_get_options_group()
+			);
+
+			wp_register_script( 'google-maps', implode( '', $google_url ), array(), GMW_VERSION, true );
+		}
+	}
+}
+
+/**
  * GMW enqueue scripts and styles
  *
  * Note, some additional script / styles enqueue in class-gmw-maps-api.php file.
@@ -12,97 +51,68 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function gmw_enqueue_scripts() {
 
-	$gmw_options    = gmw_get_options_group();
-	$protocol       = is_ssl() ? 'https' : 'http';
-	$api_key_status = true;
-	$api_key_option = gmw_get_option( 'general_settings', 'google_maps_api_usage', 'enabled' );
+	$gmw_options      = gmw_get_options_group();
+	$maps_provider    = GMW()->maps_provider;
+	$geocode_provider = GMW()->geocoding_provider;
+	$main_scripts     = array( 'jquery' );
+	$map_scripts      = array( 'jquery', 'gmw' );
+	$lf_scripts       = array( 'jquery', 'gmw' );
 
-	// enabled/ disable API key registration
-	if ( empty( $api_key_option ) || 'enabled' == $api_key_option ) {
+	// load maps provider
+	if ( 'google_maps' == $maps_provider ) {
 
-		$api_key_status = true;
+		$main_scripts[] = 'google-maps';
+		gmw_register_google_maps_api();
 
-	} elseif ( 'disabled' == $api_key_option || ( IS_ADMIN && 'admin' == $api_key_option ) || ( ! IS_ADMIN && 'frontend' == $api_key_option ) ) {
+	} else if ( 'leaflet' == $maps_provider ) {
 
-		$api_key_status = false;
-	}
+		$map_scripts[] = $lf_scripts[] = 'leaflet';
+		wp_register_script( 'leaflet', GMW_URL . '/assets/lib/leaflet/leaflet.bundle.min.js', array(), GMW_VERSION, true );
 
-	//register google maps api. Can be disabled via filter other than the settings.
-	if ( apply_filters( 'gmw_google_maps_api', true ) && $api_key_status ) {
-
-		$handle = 'google-maps';
-
-		if ( ! wp_script_is( 'google-maps', 'registered' ) ) {
-
-			//Build Google API url. elements can be modified via filters
-			$google_url = apply_filters(
-				'gmw_google_maps_api_url', array(
-					'protocol' => $protocol,
-					'url_base' => '://maps.googleapis.com/maps/api/js?',
-					'url_data' => http_build_query(
-						apply_filters(
-							'gmw_google_maps_api_args', array(
-								'libraries' => 'places',
-								'key'       => gmw_get_option( 'general_settings', 'google_api', '' ),
-								'region'    => gmw_get_option( 'general_settings', 'country_code', 'US' ),
-								'language'  => gmw_get_option( 'general_settings', 'language_code', 'EN' ),
-							)
-						), '', '&amp;'
-					),
-				), $gmw_options
-			);
-
-			wp_register_script( 'google-maps', implode( '', $google_url ), array(), GMW_VERSION, true );
-		}
-
-		// if Maps API registration is disabled, the google maps handle can be modified
-		// in case that another plugin loads the Maps API
 	} else {
-
-		$handle = apply_filters( 'gmw_google_maps_api_handle', false );
+		do_action( 'gmw_register_maps_provider_' . $maps_provider );
 	}
+
+	// load geocoding providers.
+	if ( 'google_maps' == $geocode_provider ) {
+
+		// Load geocoding provider only if other than Google Maps.
+		// Otherwise, no need to load Google Maps again as it is already loaded as map provider.
+		if ( 'google_maps' != $maps_provider ) {
+			$main_scripts[] = 'google-maps';
+			gmw_register_google_maps_api();
+		};
+
+	// Load custom geocoding provider.
+	} else {
+		do_action( 'gmw_register_geocoding_provider_' . $geocode_provider );
+	}
+
+	//$main_scripts = apply_filters( 'gmw_main_script_dependencies', $main_scripts );
 
 	// register gmw script
-	wp_register_script(
-		'gmw',
-		GMW_URL . '/assets/js/gmw.min.js',
-		! empty( $handle ) ? array( 'jquery', $handle ) : array( 'jquery' ),
-		GMW_VERSION,
-		true
+	wp_register_script( 'gmw', GMW_URL . '/assets/js/gmw.core.min.js', $main_scripts, GMW_VERSION, true );
+
+	// Variables to localize as JavaScript.
+	$options = array(
+		'settings'          => array(
+			'general' 		=> $gmw_options['general_settings'],
+			'api'           => $gmw_options['api_providers'],
+		),
+		'mapsProvider'      => $maps_provider,
+		'geocodingProvider' => $geocode_provider,
+		'isAdmin'           => IS_ADMIN,
+		'ajaxUrl'           => GMW()->ajax_url,
 	);
 
-	// ajax url
+	wp_localize_script( 'gmw', 'gmwVars', $options );
+
+	// deprecated.
 	wp_localize_script( 'gmw', 'gmwAjaxUrl', GMW()->ajax_url );
 
-	// check if in admin or front-end
-	wp_localize_script( 'gmw', 'gmwIsAdmin', array( IS_ADMIN ) );
-
-	$js_geocoder = ( apply_filters( 'gmw_client_side_geocoder_enabled', true ) ) ? array( true ) : array( false );
-
-	wp_localize_script( 'gmw', 'clientSideGeocoder', $js_geocoder );
-
-	/**
-	 * localize gmw options
-	 *
-	 * For now it seems that we don't need to pass all the settings
-	 *
-	 * So we only pass the general settings.
-	 *
-	 * This can be changed in the future if needed.
-	 **/
-	$general_settings = $gmw_options['general_settings'];
-
-	// remove the API key from the localize settings.
-	unset( $general_settings['google_api'] );
-
-	wp_localize_script( 'gmw', 'gmwSettings', array( 'general_settings' => $general_settings ) );
-
-	// fonts
-	wp_enqueue_style( 'gmw-fonts', GMW_URL . '/assets/css/gmw.font.min.css', array(), GMW_VERSION );
-
-	// register location form scripts/styles
+	// register location form.
 	wp_register_style( 'gmw-location-form', GMW_URL . '/includes/location-form/assets/css/gmw.location.form.min.css', array(), GMW_VERSION );
-	wp_register_script( 'gmw-location-form', GMW_URL . '/includes/location-form/assets/js/gmw.location.form.min.js', array( 'jquery', 'gmw' ), GMW_VERSION, true );
+	wp_register_script( 'gmw-location-form', GMW_URL . '/includes/location-form/assets/js/gmw.location.form.min.js', $lf_scripts, GMW_VERSION, true );
 
 	// register in front-end only
 	if ( ! IS_ADMIN ) {
@@ -110,8 +120,8 @@ function gmw_enqueue_scripts() {
 		// include GMW main stylesheet
 		wp_enqueue_style( 'gmw-frontend', GMW_URL . '/assets/css/gmw.frontend.min.css', array(), GMW_VERSION );
 
-		//register gmw map script
-		wp_register_script( 'gmw-map', GMW_URL . '/assets/js/gmw.map.min.js', array( 'gmw' ), GMW_VERSION, true );
+		// Map script.
+		wp_register_script( 'gmw-map', GMW_URL . '/assets/js/gmw.map.min.js', $map_scripts, GMW_VERSION, true );
 
 		// load styles in head
 		$form_styles = apply_filters( 'gmw_load_form_styles_in_head', array() );
@@ -123,9 +133,11 @@ function gmw_enqueue_scripts() {
 			}
 		}
 
-		//register scripts/styles in admin only
+	//register scripts/styles in admin only
 	} else {
 
+		// fonts file in admin only. In front-end it is combined with front-end stylesheet.
+		wp_enqueue_style( 'gmw-fonts', GMW_URL . '/assets/css/gmw.font.min.css', array(), GMW_VERSION );
 		wp_enqueue_style( 'gmw-admin', GMW_URL . '/assets/css/gmw.admin.min.css', array(), GMW_VERSION );
 
 		// enqueue on GMW admin pages only
