aef6f0c90624bb8db34eb76aa3765452b0ca69c2
Eyal Fitoussi <info@geomywp.com>
2018-10-22 21:22:53 -1000

    - escape values.
    - wpcs.

--- includes/widgets/class-gmw-search-form-widget.php
+++ includes/widgets/class-gmw-search-form-widget.php
@@ -1,37 +1,35 @@
 <?php
-// Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-        
+
 /**
  * GEO my WP search form widget.
  *
  * @since 1.0
  * @modified 3.0
- * 
  */
 class GMW_Search_Form_Widget extends GMW_Widget {
 
 	/**
 	 * Widget ID
-	 * 
+	 *
 	 * @var string
 	 */
 	public $widget_id = 'gmw_search_form_widget';
 
 	/**
 	 * Widget class
-	 * 
+	 *
 	 * @var string
 	 */
 	public $widget_class = 'geo-my-wp widget-search-form';
-	
+
 	/**
 	 * Constructor.
 	 */
 	public function __construct() {
-		
+
 		$this->widget_description = __( 'Display GEO my WP search form.', 'geo-my-wp' );
 		$this->widget_name        = __( 'GEO my WP Search Form', 'geo-my-wp' );
 
@@ -40,33 +38,36 @@ class GMW_Search_Form_Widget extends GMW_Widget {
 		if ( empty( $forms ) || ! is_array( $forms ) ) {
 			$forms = array();
 		}
-		
+
 		$options = array();
-		// allowed forms in this widget
-		$allowed_forms = apply_filters( 'gmw_search_form_widget_allowed_forms', array( 'posts_locator', 'members_locator','bp_groups_locator', 'users_locator' ) );
-		
+
+		// allowed forms in this widget.
+		$allowed_forms = apply_filters( 'gmw_search_form_widget_allowed_forms', array( 'posts_locator', 'members_locator', 'bp_groups_locator', 'users_locator' ) );
+
 		foreach ( $forms as $form ) {
 
-			if ( ( $form_id = absint( $form['ID'] ) ) != FALSE && in_array( $form['slug'], $allowed_forms ) ) {
+			$form_id = absint( $form['ID'] );
 
-				$form_name  = ! empty( $form['name'] ) ? $form['name'] : 'form_id_'.$form_id;
-				$form_label = 'Form ID '.$form_id.' ( '.$form_name.' )';
-				$options[$form_id] = $form_label;
+			if ( ! empty( $form_id ) && in_array( $form['slug'], $allowed_forms, true ) ) {
+
+				$form_name           = ! empty( $form['name'] ) ? $form['name'] : 'form_id_' . $form_id;
+				$form_label          = 'Form ID ' . $form_id . ' ( ' . $form_name . ' )';
+				$options[ $form_id ] = $form_label;
 			}
-		} 
+		}
 
 		$this->settings = array(
-			'title' 	=> array(
+			'title'   => array(
 				'type'    => 'text',
 				'default' => __( 'Search Locations', 'geo-my-wp' ),
 				'label'   => __( 'Title', 'geo-my-wp' ),
 			),
-			'form_id' => array( 
+			'form_id' => array(
 				'type'    => 'select',
 				'default' => '',
 				'label'   => __( 'Form', 'geo-my-wp' ),
-				'options' => $options
-			)
+				'options' => $options,
+			),
 		);
 
 		$this->register();
@@ -76,33 +77,33 @@ class GMW_Search_Form_Widget extends GMW_Widget {
 	 * Echoes the widget content.
 	 *
 	 * @see WP_Widget
-	 * @param array $args
-	 * @param array $instance
+	 *
+	 * @param array $args     array of args.
+	 * @param array $instance array of values.
 	 */
 	public function widget( $args, $instance ) {
 
 		ob_start();
 
-		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base );
+		$title = ! empty( $title ) ? esc_html( $title ) : '';
 
-		$title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
-		$title = apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base );		
-		
-		echo $before_widget;
+		echo $args['before_widget']; // WPCS: XSS ok.
 
-		if ( $title ) { echo $before_title . esc_attr( $title ) . $after_title; } 
-		
-		// verify shortcode ID
-		if ( absint( $instance['form_id'] ) != False ) {
+		if ( $title ) {
+			echo $args['before_title'] . $title . $args['after_title']; // WPCS: XSS ok.
+		}
+
+		// verify shortcode ID.
+		if ( false !== absint( $instance['form_id'] ) ) {
 			echo do_shortcode( "[gmw search_form=\"{$instance['form_id']}\" widget=\"1\"]" );
-		}		
-			
-		echo $after_widget;
+		}
+
+		echo $args['after_widget']; // WPCS: XSS ok.
 
 		$content = ob_get_clean();
 
-		echo $content;
+		echo $content; // WPCS: XSS ok.
 	}
 }
 register_widget( 'GMW_Search_Form_Widget' );
-?>
