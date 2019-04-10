2a7b0c3a806b1c3772e69fd4869b5f259fae2bd3
Eyal Fitoussi <info@geomywp.com>
2019-04-09 16:15:10 -1000

    Tweak: save radius value if exists when updating location.

--- includes/location-form/includes/class-gmw-location-form.php
+++ includes/location-form/includes/class-gmw-location-form.php
@@ -1264,7 +1264,7 @@ class GMW_Location_Form {
 		$location['map_icon'] = ! empty( $location['map_icon'] ) ? $location['map_icon'] : '_default.png';
 
 		// radius if exists
-		//$location['radius'] = ! empty( $location['radius'] ) ? $location['radius'] : 0.0;
+		$location['radius'] = ! empty( $location['radius'] ) ? $location['radius'] : 0.0;
 
 		$location_args = array(
 			'ID'				=> $location_id,
@@ -1293,7 +1293,7 @@ class GMW_Location_Form {
 			'formatted_address' => $location['formatted_address'],
 			'place_id'          => $location['place_id'],
 			'map_icon'          => $location['map_icon'],
-			//'radius'            => $location['radius'],
+			'radius'            => $location['radius'],
 		);
 
 		// filter location args before updating location
