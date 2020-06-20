0bf1d39832eb664f6221eee45cb840e544bad22f
Eyal Fitoussi <info@geomywp.com>
2020-06-19 14:32:17 -1000

    New version tag

--- geo-my-wp.php
+++ geo-my-wp.php
@@ -7,7 +7,7 @@
  * Author: Eyal Fitoussi
  * Author URI: http://www.geomywp.com
  * Requires at least: 4.5
- * Tested up to: 5.4.2
+ * Tested up to: 5.4.1
  * Buddypress: 2.8 or higher
  * Text Domain: geo-my-wp
  * Domain Path: /languages
