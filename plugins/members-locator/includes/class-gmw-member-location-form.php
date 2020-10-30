2bc769628d49c772f8535feeaa3b426d04015848
Eyal Fitoussi <info@geomywp.com>
2020-10-29 15:54:08 -1000

    WPCS

--- plugins/members-locator/includes/class-gmw-member-location-form.php
+++ plugins/members-locator/includes/class-gmw-member-location-form.php
@@ -1,5 +1,15 @@
 <?php
-// Exit if accessed directly
+/**
+ * GEO my WP Member Location Form Class.
+ *
+ * Generates the proximity search forms.
+ *
+ * This class should be extended for different object types.
+ *
+ * @package geo-my-wp
+ */
+
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -14,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 class GMW_Member_Location_Form extends GMW_Location_Form {
 
 	/**
-	 * addon
+	 * Addon slug
 	 *
 	 * @var string
 	 */
@@ -28,15 +38,15 @@ class GMW_Member_Location_Form extends GMW_Location_Form {
 	public $object_type = 'user';
 
 	/**
-	 * Run the form class
+	 * [__construct description]
 	 *
-	 * @param array $attr [description]
+	 * @param array $attr [description].
 	 */
 	public function __construct( $attr = array() ) {
 
 		parent::__construct( $attr );
 
-		// add custom tab panels
+		// add custom tab panels.
 		add_action( 'gmw_lf_content_end', array( $this, 'create_tabs_panels' ) );
 	}
 
@@ -59,7 +69,7 @@ class GMW_Member_Location_Form extends GMW_Location_Form {
 	 */
 	public function form_fields() {
 
-		// retreive parent fields
+		// retreive parent fields.
 		$fields = parent::form_fields();
 
 		return apply_filters( 'gmw_member_location_tab_fields', $fields, $this->args );
