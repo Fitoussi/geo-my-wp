95974088b4a42e763299816b7ee11776abbd249a
Eyal Fitoussi <info@geomywp.com>
2020-12-16 23:14:23 -1000

    WPCS escape field value.

--- includes/location-form/templates/form-fields/text-field.php
+++ includes/location-form/templates/form-fields/text-field.php
@@ -1,18 +1,18 @@
 <fieldset class="gmw-lf-field-wrapper <?php echo esc_attr( $field['id'] ); ?> <?php echo esc_attr( $field['type'] ); ?>">
-	
+
 	<?php if ( ! empty( $field['label'] ) ) { ?>
-		
+
 		<label for="<?php echo esc_attr( $field['id'] ); ?>">
 			<?php echo esc_attr( $field['label'] ); ?>	
 		</label>
-	
+
 	<?php } ?>
 
 	<input type="text" class="<?php echo esc_attr( $field['class'] ); ?>" id="<?php echo esc_attr( $field['id'] ); ?>" placeholder="<?php echo esc_attr( $field['placeholder'] ); ?>" name="<?php echo esc_attr( $field_name ); ?>" value="<?php echo sanitize_text_field( esc_attr( $field['value'] ) ); ?>" <?php echo esc_attr( $field['attributes'] ); ?> />
-	
+
 	<?php if ( ! empty( $field['desc'] ) ) { ?>
 		<em class="description">
-			<?php echo $field['desc']; ?>	
+			<?php echo esc_html( $field['desc'] ); ?>
 		</em>
 	<?php } ?>
 </fieldset>
