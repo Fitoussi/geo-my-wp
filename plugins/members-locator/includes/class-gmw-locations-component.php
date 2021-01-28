4c9e1b74a3b3a787dc382ade02c95b172731e76d
Eyal Fitoussi <info@geomywp.com>
2021-01-27 14:30:54 -1000

    WPCS

--- plugins/members-locator/includes/class-gmw-locations-component.php
+++ plugins/members-locator/includes/class-gmw-locations-component.php
@@ -1,10 +1,16 @@
 <?php
-// Exit if accessed directly
+/**
+ * GEO my WP - Members Locator BP Component.
+ *
+ * @package geo-my-wp
+ */
+
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// load Members Locator component
+// load Members Locator component.
 if ( class_exists( 'GMW_Locations_Component' ) ) {
 	return;
 }
@@ -45,30 +51,13 @@ class GMW_Members_Locator_Component extends BP_Component {
 	}
 
 	/**
-	 * GMW location files
+	 * Setup globals.
 	 *
-	 * @package GMW Location
-	 * @since 1.0
-	 */
-	/*
-	public function includes( $includes = array() ) {
-
-		// include files
-		$includes = array(
-			'includes/gmw-members-locator-actions.php',
-			'includes/gmw-member-locator-activity.php'
-		);
-
-		parent::includes( $includes );
-	}
-	*/
-
-	/**
-	 * GMW Location globals
+	 * @param  array $args arguments.
 	 */
 	public function setup_globals( $args = array() ) {
 
-		// Set up the $globals array to be passed along to parent::setup_globals()
+		// Set up the $globals array to be passed along to parent::setup_globals().
 		$globals = array(
 			'slug'                  => $this->slug,
 			'root_slug'             => isset( $bp->pages->{$this->id}->slug ) ? $bp->pages->{$this->id}->slug : $this->slug,
@@ -83,25 +72,27 @@ class GMW_Members_Locator_Component extends BP_Component {
 	/**
 	 * Setup Location tab
 	 *
-	 * @param  array $main_nav [description]
-	 * @param  array $sub_nav  [description]
-	 * @return [type]           [description]
+	 * @param  array $main_nav [description].
+	 *
+	 * @param  array $sub_nav  [description].
 	 */
 	public function setup_nav( $main_nav = array(), $sub_nav = array() ) {
 
 		if ( gmw_is_addon_active( 'members_locator' ) ) {
 
-			include( GMW_FL_PATH . '/includes/gmw-members-locator-location-screen.php' );
+			include GMW_FL_PATH . '/includes/gmw-members-locator-location-screen.php';
 
-			// Add 'location' to the main navigation
+			// Add 'location' to the main navigation.
 			$main_nav = apply_filters(
-				'gmw_fl_setup_nav', array(
+				'gmw_fl_setup_nav',
+				array(
 					'name'                => __( 'Location', 'geo-my-wp' ),
 					'slug'                => $this->slug,
 					'position'            => 60,
 					'screen_function'     => 'gmw_fl_screen_display_location',
 					'default_subnav_slug' => $this->slug,
-				), buddypress()->displayed_user
+				),
+				buddypress()->displayed_user
 			);
 		}
 
@@ -111,22 +102,22 @@ class GMW_Members_Locator_Component extends BP_Component {
 	/**
 	 * Location item in admin bar menu
 	 *
-	 * @param  array $wp_admin_nav [description]
-	 * @return [type]               [description]
+	 * @param  array $wp_admin_nav [description].
 	 */
 	public function setup_admin_bar( $wp_admin_nav = array() ) {
 
 		if ( gmw_is_addon_active( 'members_locator' ) && is_user_logged_in() ) {
 
-			// Prevent debug notices
+			// Prevent debug notices.
 			$wp_admin_nav = array();
 
-			// Setup the logged in user variables
+			// Setup the logged in user variables.
 			$location_link = trailingslashit( bp_loggedin_user_domain() . $this->slug );
 
-			// Add location tab
+			// Add location tab.
 			$wp_admin_nav[] = apply_filters(
-				'gmw_fl_setup_admin_bar', array(
+				'gmw_fl_setup_admin_bar',
+				array(
 					'parent' => 'my-account-buddypress',
 					'id'     => 'my-account-' . $this->slug,
 					'title'  => __( 'Location', 'geo-my-wp' ),
@@ -134,7 +125,7 @@ class GMW_Members_Locator_Component extends BP_Component {
 				)
 			);
 
-			// add submenu tab
+			// add submenu tab.
 			$wp_admin_nav[] = array(
 				'parent' => 'my-account-' . $this->slug,
 				'id'     => 'my-account-' . $this->slug . '-gmw-location',
