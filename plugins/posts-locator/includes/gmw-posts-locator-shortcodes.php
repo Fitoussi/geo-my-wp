310220df656bc50abda9fe05788af3e94decd922
Eyal Fitoussi <info@geomywp.com>
2022-02-19 00:24:48 -1000

    Tweak: new functions to generate the 'gmw_post_address' and 'gmw_post_location_fields' shortcodes.

--- plugins/posts-locator/includes/gmw-posts-locator-shortcodes.php
+++ plugins/posts-locator/includes/gmw-posts-locator-shortcodes.php
@@ -36,13 +36,45 @@ add_shortcode( 'gmw_post_location_form', 'gmw_post_location_form_shortcode' );
 /**
  * Output the location address fields of a post.
  *
+ * @param  array $atts shortcode attributes.
+ *
  * @see gmw_get_post_address() for the list of attributes ( includes/gmw-posts-locator-functions.php ).
  */
-add_shortcode( 'gmw_post_address', 'gmw_get_post_address' );
+function gmw_get_post_address_shortcode( $atts = array() ) {
+
+	if ( empty( $atts ) ) {
+		$atts = array();
+	}
+
+	ob_start();
+
+	echo gmw_get_post_address( $atts ); // WPCS: XSS ok.
+
+	$content = ob_get_clean();
+
+	return $content;
+}
+add_shortcode( 'gmw_post_address', 'gmw_get_post_address_shortcode' );
 
 /**
  * Output post location fields or location meta fields.
  *
+ * @param  array $atts shortcode attributes.
+ *
  * @see gmw_get_post_location_fields() for the list of attributes ( includes/gmw-posts-locator-functions.php ).
  */
-add_shortcode( 'gmw_post_location_fields', 'gmw_get_post_location_fields' );
+function gmw_get_post_location_fields_shortcode( $atts = array() ) {
+
+	if ( empty( $atts ) ) {
+		$atts = array();
+	}
+
+	ob_start();
+
+	echo gmw_get_post_location_fields( $atts ); // WPCS: XSS ok.
+
+	$content = ob_get_clean();
+
+	return $content;
+}
+add_shortcode( 'gmw_post_location_fields', 'gmw_get_post_location_fields_shortcode' );
