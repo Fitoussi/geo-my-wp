f96db504e2056997a60c57474d1d31ba53fa2224
Eyal Fitoussi <info@geomywp.com>
2022-02-18 18:58:25 -1000

    NEw: members locator mashup map class.

---
+++ plugins/members-locator/includes/class-gmw-members-locator-mashup-map-form.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * GEO my WP Members Locator mashup Map form class.
+ *
+ * @package geo-my-wp
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Posts Locator mashup map form class.
+ *
+ * @since 4.0.
+ *
+ * @package geo-my-wp
+ */
+class GMW_Members_Locator_Mashup_Map_Form extends GMW_Members_Locator_Form {
+
+	/**
+	 * __construct function.
+	 *
+	 * @param array $form gmw form.
+	 */
+	public function __construct( $form ) {
+
+		// If passing form shortcode attribute, convert it to map.
+		if ( 'map' !== $form['current_element'] ) {
+
+			$form['current_element'] = 'map';
+			$form['params']['map']   = $form['ID'];
+		}
+
+		parent::__construct( $form );
+	}
+}
