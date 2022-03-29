72d6bfc493d3d32dca638e65b69da87a0970147e
Eyal Fitoussi <info@geomywp.com>
2022-03-28 17:07:42 -1000

    New: Buddyboss template files for Posts Locator.

---
+++ plugins/posts-locator/templates/search-forms/buddyboss/content.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * GEO my WP Search Form Template. 
+ *
+ * To modify this template file, copy this folder with all its content and place it
+ *
+ * in the theme's or child theme's folder of your site under:
+ *
+ * your-theme's-or-child-theme's-folder/geo-my-wp/posts-locator/search-forms/
+ *
+ * You will then be able to select your custom template from the "Search Form Templates" select dropdown option in the "Search Form" tab of the form editor.
+ *
+ * It will be named as "Custom: %folder-name%".
+ *
+ * @param $gmw_form ( object ) the entire form object.
+ *
+ * @param $gmw      ( array )  GEO my WP's form.
+ *
+ * @author Eyal Fitoussi
+ *
+ * @package gmw-my-wp
+ */
+
+?>
+<?php do_action( 'gmw_before_search_form_template', $gmw ); ?>
+
+<div class="<?php echo esc_attr( $gmw_form->get_class_attr( 'form_wrap' ) ); ?>">
+
+	<?php do_action( 'gmw_before_search_form', $gmw ); ?>
+
+	<form class="gmw-form gmw-grid-filters-wrapper" name="gmw_form" action="<?php echo esc_attr( $gmw_form->get_results_page() ); ?>" method="get" data-id="<?php echo absint( $gmw['ID'] ); ?>" data-prefix="<?php echo esc_attr( $gmw['prefix'] ); ?>">
+
+		<?php do_action( 'gmw_search_form_start', $gmw ); ?>
+
+		<?php gmw_search_form_keywords_field( $gmw ); ?>
+
+		<?php gmw_search_form_address_field( $gmw ); ?>
+
+		<?php gmw_search_form_radius( $gmw ); ?>
+	
+		<?php gmw_search_form_units( $gmw ); ?>
+
+		<?php gmw_search_form_additional_filters_wrapper( 'open', $gmw ); ?>
+
+		<?php do_action( 'gmw_search_form_filters_start', $gmw ); ?>
+
+		<?php gmw_search_form_locator_button( $gmw ); ?>
+
+		<?php gmw_search_form_post_types( $gmw ); ?>
+
+		<?php gmw_search_form_taxonomies( $gmw ); ?>
+
+		<?php gmw_search_form_custom_fields( $gmw ); ?>
+
+		<?php do_action( 'gmw_search_form_filters_end', $gmw ); ?>
+
+		<?php gmw_search_form_additional_filters_wrapper( 'close', $gmw ); ?>
+
+		<?php gmw_search_form_additional_filters_toggle( $gmw ); ?>
+
+		<?php gmw_search_form_reset_button( $gmw ); ?>
+
+		<?php gmw_search_form_submit_button( $gmw ); ?>
+
+		<?php do_action( 'gmw_search_form_end', $gmw ); ?>
+
+	</form>
+
+	<?php do_action( 'gmw_after_search_form', $gmw ); ?>
+
+</div>	
+
+<?php do_action( 'gmw_after_search_form_template', $gmw ); ?>
---
+++ plugins/posts-locator/templates/search-results/buddyboss/single-result.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * GEO my WP Search Results Template.
+ *
+ * To modify this template file, copy this folder with all its content and place it
+ *
+ * in the theme's or child theme's folder of your site under:
+ *
+ * your-theme's-or-child-theme's-folder/geo-my-wp/posts-locator/search-results/
+ *
+ * You will then be able to select your custom template from the "Search Results Templates" select dropdown option in the "Search Results" tab of the form editor.
+ *
+ * It will be named as "Custom: %folder-name%".
+ *
+ * @param $gmw  ( array ) the form being used
+ *
+ * @param $gmw_form ( object ) the form object
+ *
+ * @param $post ( object ) post object in the loop
+ *
+ * @package geo-my-wp
+ */
+
+?>
+<article id="gmw-single-post-<?php echo absint( $post->ID ); ?>" <?php post_class( explode( ' ', $post->location_class ) ); ?>>
+
+	<div class="post-inner-wrap">
+
+		<?php gmw_search_results_distance( $post, $gmw ); ?>
+
+		<?php 
+		if ( ! empty( $gmw['search_results']['image']['enabled'] ) ) {
+			buddyboss_theme_entry_header( $post );
+		}
+		?>
+
+		<div class="entry-content-wrap">
+
+			<header class="entry-header">
+		
+				<h2 class="gmw-item gmw-item-title entry-title">
+					<?php gmw_search_results_linked_title( get_permalink(), get_the_title(), $post, $gmw ); ?>
+					<?php gmw_search_results_address( $post, $gmw ); ?>
+				</h2>
+
+				<?php if ( has_post_format( 'link' ) && function_exists( 'buddyboss_theme_get_first_url_content' ) && ( $first_url = buddyboss_theme_get_first_url_content( $post->post_content ) ) != "" ):?>
+					<p class="post-main-link"><?php echo $first_url;?></p>
+				<?php endif; ?>
+
+			</header>
+
+			<?php do_action( 'gmw_search_results_loop_content_start', $post, $gmw ); ?>
+
+			<div class="entry-content">
+				<?php gmw_search_results_post_excerpt( $post, $gmw ); ?>
+			</div>
+
+			<?php gmw_search_results_location_meta( $post, $gmw ); ?>
+
+			<?php gmw_search_results_hours_of_operation( $post, $gmw ); ?>
+
+			<?php gmw_search_results_taxonomies( $post, $gmw ); ?>
+
+			<?php gmw_search_results_directions_link( $post, $gmw ); ?>
+
+			<?php do_action( 'gmw_search_results_loop_content_end', $post, $gmw ); ?>
+
+			<?php
+			if ( ! has_post_format( 'quote' ) ) {
+				get_template_part( 'template-parts/entry-meta' );
+			}
+			?>
+		</div>
+
+	</div>
+
+</article>
