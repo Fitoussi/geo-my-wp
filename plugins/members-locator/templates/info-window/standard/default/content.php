8348795602a4de9d23de536cd04e4764524cbd28
Eyal Fitoussi <info@geomywp.com>
2022-03-28 17:07:09 -1000

    Tweak: info-window template files moved from premium extensions into core extensions.

---
+++ plugins/members-locator/templates/info-window/standard/default/content.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * GEO my WP Info-Window Template. 
+ *
+ * To modify this template file, copy this folder with all its content and place it
+ *
+ * in the theme's or child theme's folder of your site under:
+ *
+ * your-theme's-or-child-theme's-folder/geo-my-wp/members-locator/info-window/standard/
+ *
+ * You will then be able to select your custom template from the "Templates" select dropdown option in the "Info Window" tab of the form editor.
+ *
+ * It will be named as "Custom: %folder-name%".
+ *
+ * @param $gmw    ( array )  GEO my WP's form.
+ *
+ * @param $member ( object ) the memebr's object.
+ *
+ * @author Eyal Fitoussi
+ *
+ * @package gmw-my-wp
+ */
+
+?>
+<?php do_action( 'gmw_info_window_before', $member, $gmw ); ?>
+
+<div class="gmw-info-window-inner standard">
+
+	<?php do_action( 'gmw_info_window_start', $member, $gmw ); ?>
+
+	<div class="gmw-item-header">
+		<?php gmw_search_results_bp_avatar( $member, $gmw, 'info_window' ); ?>
+	</div>
+
+	<div class="gmw-item-content">
+	
+		<h3 class="gmw-item gmw-item-title">
+			<?php gmw_search_results_linked_title( bp_get_member_permalink(), bp_get_member_name(), $member, $gmw ); ?>
+			<?php gmw_search_results_bp_last_active( $member, $gmw, 'info_window' ); ?>
+		</h3>
+
+		<?php do_action( 'gmw_info_window_content_start', $member, $gmw ); ?>
+
+		<?php gmw_info_window_address( $member, $gmw ); ?>
+
+		<?php gmw_info_window_location_meta( $member, $gmw ); ?>
+
+		<?php gmw_info_window_hours_of_operation( $member, $gmw ); ?>
+
+		<?php gmw_info_window_directions_link( $member, $gmw ); ?>
+
+		<?php gmw_search_results_bp_friendship_button( $member, $gmw, 'info_window' ); ?>
+
+		<?php do_action( 'gmw_info_window_content_end', $member, $gmw ); ?>
+	</div>
+
+	<?php do_action( 'gmw_info_window_end', $member, $gmw ); ?>  	    
+</div>  
+
+<?php do_action( 'gmw_info_window_after', $member, $gmw ); ?>
---
+++ plugins/posts-locator/templates/info-window/standard/default/content.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * GEO my WP Info-Window Template. 
+ *
+ * To modify this template file, copy this folder with all its content and place it
+ *
+ * in the theme's or child theme's folder of your site under:
+ *
+ * your-theme's-or-child-theme's-folder/geo-my-wp/posts-locator/info-window/standard/
+ *
+ * You will then be able to select your custom template from the "Templates" select dropdown option in the "Info Window" tab of the form editor.
+ *
+ * It will be named as "Custom: %folder-name%".
+ *
+ * @param $gmw  ( array )  GEO my WP's form.
+ *
+ * @param $post ( object ) the post's object.
+ *
+ * @author Eyal Fitoussi
+ *
+ * @package gmw-my-wp
+ */
+
+?>
+<?php do_action( 'gmw_info_window_before', $post, $gmw ); ?> 
+
+<div class="gmw-info-window-inner standard">
+
+	<?php do_action( 'gmw_info_window_start', $post, $gmw ); ?>
+
+	<div class="gmw-item-header">
+		<?php gmw_info_window_featured_image( $post, $gmw ); ?>
+	</div>
+
+	<div class="gmw-item-content">
+
+		<h3 class="gmw-item gmw-item-title">
+			<?php gmw_info_window_linked_title( get_permalink( $post->ID ), $post->post_title, $post, $gmw ); ?>
+		</h3>
+
+		<?php do_action( 'gmw_info_window_content_start', $post, $gmw ); ?>
+
+		<?php gmw_info_window_distance( $post, $gmw ); ?>
+
+		<?php gmw_info_window_address( $post, $gmw ); ?>
+
+		<?php gmw_info_window_post_excerpt( $post, $gmw ); ?>
+
+		<?php gmw_info_window_location_meta( $post, $gmw, false ); ?>
+
+		<?php gmw_info_window_hours_of_operation( $post, $gmw ); ?>
+
+		<?php gmw_search_results_taxonomies( $post, $gmw, 'info_window' ); ?>
+
+		<?php gmw_info_window_directions_link( $post, $gmw ); ?>
+
+		<?php do_action( 'gmw_info_window_content_end', $post, $gmw ); ?>
+
+	</div>
+
+	<?php do_action( 'gmw_info_window_end', $post, $gmw ); ?>
+
+</div>
+
+<?php do_action( 'gmw_info_window_after', $post, $gmw ); ?>
