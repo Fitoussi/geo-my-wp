c54d21e2e5c458c358fbd21ea9e5df6231b5497d
Eyal Fitoussi <info@geomywp.com>
2022-04-07 11:32:23 -1000

    Tweak: rename classes gmw-additional-filters to gmw-modal-box.

--- includes/template-functions/gmw-search-form-template-functions.php
+++ includes/template-functions/gmw-search-form-template-functions.php
@@ -536,7 +536,7 @@ function gmw_get_search_form_toggle_button( $gmw = array(), $args = array() ) {
 			'inner_label' => __( 'Filter', 'geo-my-wp' ),
 			'attributes'  => array(
 				'data-type'     => 'toggle',
-				'data-element'  => '.gmw-additional-filters-wrapper',
+				'data-element'  => '.gmw-modal-box-wrapper',
 				'data-duration' => 'fast',
 			),
 		)
@@ -590,6 +590,8 @@ function gmw_search_form_modal_box( $tag = 'open', $gmw = array() ) {
 		return;
 	}
 
+	$type = ! empty( $gmw['search_form']['filters_modal']['modal_type'] ) ? $gmw['search_form']['filters_modal']['modal_type'] : 'popup';
+
 	if ( 'close' === $tag ) {
 		?>
 		</div></div></div>
@@ -598,17 +600,17 @@ function gmw_search_form_modal_box( $tag = 'open', $gmw = array() ) {
 
 		$title = ! empty( $gmw['search_form']['filters_modal']['modal_title'] ) ? $gmw['search_form']['filters_modal']['modal_title'] : '';
 		?>
-		<div class="gmw-additional-filters-wrapper" data-type="popup">
+		<div class="gmw-modal-box-wrapper" data-type="<?php echo esc_attr( $type ); ?>">
 
-			<div class="gmw-additional-filters-inner">
+			<div class="gmw-modal-box-inner">
 
-				<div class="gmw-additional-filters-header">
+				<div class="gmw-modal-box-header">
 					<span></span>
-					<span class="gmw-additional-filters-title"><?php echo esc_html( $title ); ?></span>
+					<span class="gmw-modal-box-title"><?php echo esc_html( $title ); ?></span>
 					<span class="gmw-close-filters-button gmw-icon-cancel-circled"></span>
 				</div>
 
-				<div class="gmw-additional-filters-content gmw-grid-filters-wrapper">
+				<div class="gmw-modal-box-content gmw-grid-filters-wrapper">
 		<?php
 	}
 
