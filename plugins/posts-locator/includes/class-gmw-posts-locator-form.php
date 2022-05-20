35124e2261dbf6edf96990ad947fb49630946d1b
Eyal Fitoussi <info@geomywp.com>
2022-05-20 13:24:29 -1000

    Tweak: new Trait GMW_Posts_Locator_Form_Trait() that contains methods related to the search queries and can be used with the different extensions and different forms types. Enhance the search query clauses.

--- plugins/posts-locator/includes/class-gmw-posts-locator-form.php
+++ plugins/posts-locator/includes/class-gmw-posts-locator-form.php
@@ -13,6 +13,143 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * Extend the Posts Locator Form classes.
+ *
+ * @since 4.0.
+ */
+trait GMW_Posts_Locator_Form_Trait {
+
+	/**
+	 * Modify wp_query clauses to search by distance
+	 *
+	 * @param array $clauses query clauses.
+	 *
+	 * @return modified $clauses
+	 */
+	public function query_clauses( $clauses ) {
+
+		global $wpdb;
+
+		// add the location db fields to the query.
+		$fields       = ', gmw_locations.' . implode( ', gmw_locations.', $this->db_fields );
+		$having       = '';
+		$join         = "INNER JOIN {$wpdb->base_prefix}gmw_locations gmw_locations ON ( $wpdb->posts.ID = gmw_locations.object_id AND gmw_locations.object_type = 'post' ) ";
+		$where        = '';
+		$units        = '';
+		$distance_sql = "'' AS distance";
+
+		// In multisite we need to check for the blog ID.
+		if ( is_multisite() && ! empty( $wpdb->blogid ) ) {
+			$blog_id = absint( $wpdb->blogid );
+			$where   = " AND gmw_locations.blog_id = {$blog_id} ";
+		}
+
+		// get address filters query.
+		$address_filters = gmw_get_address_fields_filters_sql( $this->get_address_filters(), $this->form );
+
+		// search within map bounderies.
+		if ( ! empty( $this->form['form_values']['nelatlng'] ) && ! empty( $this->form['form_values']['swlatlng'] ) ) {
+
+			$where .= gmw_get_locations_within_bounderies_sql( $this->form['form_values']['swlatlng'], $this->form['form_values']['nelatlng'] );
+
+		// when address provided, and not filtering based on address fields, we will do proximity search.
+		} elseif ( empty( $address_filters ) && ! empty( $this->form['lat'] ) && ! empty( $this->form['lng'] ) ) {
+
+			// generate some radius/units data.
+			if ( 'imperial' === $this->form['units'] ) {
+				$earth_radius = 3959;
+				$units        = 'mi';
+				$degree       = 69.0;
+			} else {
+				$earth_radius = 6371;
+				$units        = 'km';
+				$degree       = 111.045;
+			}
+
+			// since these values are repeatable, we escape them previous
+			// the query instead of running multiple prepares.
+			$lat      = esc_sql( $this->form['lat'] );
+			$lng      = esc_sql( $this->form['lng'] );
+			$distance = ! empty( $this->form['radius'] ) ? esc_sql( $this->form['radius'] ) : '';
+
+			$distance_sql = "ROUND( {$earth_radius} * acos( cos( radians( {$lat} ) ) * cos( radians( gmw_locations.latitude ) ) * cos( radians( gmw_locations.longitude ) - radians( {$lng} ) ) + sin( radians( {$lat} ) ) * sin( radians( gmw_locations.latitude ) ) ),1 ) AS distance";
+
+			if ( ! empty( $distance ) ) {
+
+				if ( ! apply_filters( 'gmw_disable_query_clause_between', false, 'gmw_' . $this->form['prefix'], $this->form ) ) {
+
+					// calculate the between point.
+					$bet_lat1 = $lat - ( $distance / $degree );
+					$bet_lat2 = $lat + ( $distance / $degree );
+					$bet_lng1 = $lng - ( $distance / ( $degree * cos( deg2rad( $lat ) ) ) );
+					$bet_lng2 = $lng + ( $distance / ( $degree * cos( deg2rad( $lat ) ) ) );
+
+					$where .= " AND gmw_locations.latitude BETWEEN {$bet_lat1} AND {$bet_lat2}";
+					// $clauses['where'] .= " AND gmw_locations.longitude BETWEEN {$bet_lng1} AND {$bet_lng2} ";
+				}
+
+				// filter locations based on the distance.
+				$having = "HAVING distance <= {$distance} OR distance IS NULL";
+			}
+
+			// Remove extra spaces.
+			$this->form['query_args']['orderby'] = ! empty( $this->form['query_args']['orderby'] ) ? trim( $this->form['query_args']['orderby'] ) : 'distance';
+
+			// If there is another order-by parameter before the distance append the distance to the orderby clause.
+			if ( false !== strpos( $this->form['query_args']['orderby'], ' distance' ) ) {
+
+				$clauses['orderby'] .= ', distance ASC';
+
+				// If there is another order-by parameter after the distance, prepend the distance first in the orderby clause.
+			} elseif ( false !== strpos( $this->form['query_args']['orderby'], 'distance ' ) ) {
+
+				$clauses['orderby'] = 'distance ASC, ' . $clauses['orderby'];
+
+				// Otherise, we order by the distance only.
+			} elseif ( 'distance' === $this->form['query_args']['orderby'] ) {
+
+				$clauses['orderby'] = ! empty( $this->form['query_args']['order'] ) ? 'distance ' . $this->form['query_args']['order'] : 'distance';
+			}
+		} else {
+
+			// if showing posts without location.
+			if ( ! empty( $this->enable_objects_without_location ) ) {
+
+				// left join the location table into the query to display posts with no location as well.
+				$join = str_replace( 'INNER', 'LEFT', $join );
+
+			} else {
+
+				$where .= " AND ( gmw_locations.latitude != 0.000000 && gmw_locations.longitude != 0.000000 ) ";
+			}
+
+			$where .= ' ' . $address_filters;
+		}
+
+		$clauses['fields'] .= " {$fields}, {$distance_sql}, '{$units}' AS units";
+		$clauses['join']   .= $join;
+		$clauses['where']  .= $where;
+		$clauses['having']  = $having;
+
+		// modify the clauses.
+		$clauses = apply_filters( 'gmw_posts_locator_locations_query_clauses', $clauses, $this->form, $this );
+		$clauses = apply_filters( 'gmw_' . $this->form['prefix'] . '_location_query_clauses', $clauses, $this->form );
+
+		// make sure we have groupby to only pull posts one time.
+		if ( empty( $clauses['groupby'] ) ) {
+			$clauses['groupby'] = $wpdb->prefix . 'posts.ID';
+		}
+
+		// add having clause.
+		$clauses['groupby'] .= ' ' . $clauses['having'];
+
+		unset( $clauses['having'] );
+
+		return $clauses;
+	}
+}
+
+/**
  * Posts Locator form class.
  *
  * @package geo-my-wp
@@ -20,30 +157,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 class GMW_Posts_Locator_Form extends GMW_Form {
 
 	/**
-	 * Gmw_location database fields that will be
-	 *
-	 * Added to the posts query.
-	 *
-	 * @var array
+	 * Inherit search queries fromt Trait.
 	 */
-	/**
-	Public $db_fields = array(
-		'ID as location_id',
-		'object_type',
-		'object_id',
-		'featured as featured_location',
-		'user_id',
-		'latitude as lat',
-		'longitude as lng',
-		'street',
-		'city',
-		'region_name',
-		'postcode',
-		'country_code',
-		'address',
-		'formatted_address',
-		'map_icon',
-	); */
+	use GMW_Posts_Locator_Form_Trait;
 
 	/**
 	 * Permalink hook
@@ -91,176 +207,6 @@ class GMW_Posts_Locator_Form extends GMW_Form {
 	}
 
 	/**
-	 * Modify wp_query clauses to search by distance
-	 *
-	 * @param array $clauses query clauses.
-	 *
-	 * @return modified $clauses
-	 */
-	public function query_clauses( $clauses ) {
-
-		$count     = 0;
-		$db_fields = '';
-
-		// generate the db fields.
-		foreach ( $this->db_fields as $field ) {
-
-			if ( $count > 0 ) {
-				$db_fields .= ', ';
-			}
-
-			$count++;
-
-			if ( strpos( $field, 'as' ) !== false ) {
-
-				$field = explode( ' as ', $field );
-
-				$db_fields .= "gmw_locations.{$field[0]} as {$field[1]}";
-
-				// Here we are including latitude and longitude fields
-				// using their original field name.
-				// for backward compatibility, we also need to have "lat" and "lng"
-				// in the location object and that is what we did in the line above.
-				// The lat and lng field are too involve and need to carfully change it.
-				// eventually we want to completely move to using latitude and longitude.
-				if ( 'latitude' === $field[0] || 'longitude' === $field[0] ) {
-					$db_fields .= ",gmw_locations.{$field[0]}";
-				}
-			} else {
-
-				$db_fields .= "gmw_locations.{$field}";
-			}
-		}
-
-		global $wpdb;
-
-		$where_clause_filter = apply_filters( 'gmw_filter_object_type_in_where_clause', false, $this->form );
-
-		// add the location db fields to the query.
-		$clauses['fields'] .= ", {$db_fields}";
-		$clauses['having']  = '';
-		$tjoin              = "{$wpdb->base_prefix}gmw_locations gmw_locations ON $wpdb->posts.ID = gmw_locations.object_id ";
-
-		if ( ! $where_clause_filter ) {
-			$tjoin .= "AND gmw_locations.object_type = 'post' ";
-		}
-
-		// In multisite we need to check for the blog ID.
-		if ( is_multisite() && ! empty( $wpdb->blogid ) ) {
-			$blog_id           = absint( $wpdb->blogid );
-			$clauses['where'] .= "AND gmw_locations.blog_id = {$blog_id} ";
-		}
-
-		// get address filters query.
-		$address_filters = GMW_Location::query_address_fields( $this->get_address_filters(), $this->form );
-
-		// when address provided, and not filtering based on address fields, we will do proximity search.
-		if ( empty( $address_filters ) && ! empty( $this->form['lat'] ) && ! empty( $this->form['lng'] ) ) {
-
-			// generate some radius/units data.
-			if ( in_array( $this->form['units_array']['units'], array( 'imperial', 3959, 'miles', '3959' ), true ) ) {
-				$earth_radius = 3959;
-				$units        = 'mi';
-				$degree       = 69.0;
-			} else {
-				$earth_radius = 6371;
-				$units        = 'km';
-				$degree       = 111.045;
-			}
-
-			// add units to locations data.
-			$clauses['fields'] .= ", '{$units}' AS units";
-
-			// since these values are repeatable, we escape them previous
-			// the query instead of running multiple prepares.
-			$lat      = esc_sql( $this->form['lat'] );
-			$lng      = esc_sql( $this->form['lng'] );
-			$distance = ! empty( $this->form['radius'] ) ? esc_sql( $this->form['radius'] ) : '';
-
-			$clauses['fields'] .= ", ROUND( {$earth_radius} * acos( cos( radians( {$lat} ) ) * cos( radians( gmw_locations.latitude ) ) * cos( radians( gmw_locations.longitude ) - radians( {$lng} ) ) + sin( radians( {$lat} ) ) * sin( radians( gmw_locations.latitude ) ) ),1 ) AS distance";
-
-			$clauses['join'] .= "INNER JOIN {$tjoin}";
-
-			if ( ! empty( $distance ) ) {
-
-				if ( ! apply_filters( 'gmw_disable_query_clause_between', false, 'gmw_pt', $this->form ) ) {
-
-					// calculate the between point.
-					$bet_lat1 = $lat - ( $distance / $degree );
-					$bet_lat2 = $lat + ( $distance / $degree );
-					$bet_lng1 = $lng - ( $distance / ( $degree * cos( deg2rad( $lat ) ) ) );
-					$bet_lng2 = $lng + ( $distance / ( $degree * cos( deg2rad( $lat ) ) ) );
-
-					$clauses['where'] .= " AND gmw_locations.latitude BETWEEN {$bet_lat1} AND {$bet_lat2}";
-					// $clauses['where'] .= " AND gmw_locations.longitude BETWEEN {$bet_lng1} AND {$bet_lng2} ";
-				}
-
-				// filter locations based on the distance.
-				$clauses['having'] = "HAVING distance <= {$distance} OR distance IS NULL";
-			}
-
-			if ( $where_clause_filter ) {
-				$clauses['where'] .= " AND gmw_locations.object_type = 'post'";
-			}
-
-			// filter locations based on the distance.
-			//$clauses['having'] = "HAVING distance <= {$distance} OR distance IS NULL";
-
-			// Remove extra spaces.
-			$this->form['query_args']['orderby'] = trim( $this->form['query_args']['orderby'] );
-
-			// If there is another order-by parameter before the distance append the distance to the orderby clause.
-			if ( false !== strpos( $this->form['query_args']['orderby'], ' distance' ) ) {
-
-				$clauses['orderby'] .= ', distance ASC';
-
-				// If there is another order-by parameter after the distance, prepend the distance first in the orderby clause.
-			} elseif ( false !== strpos( $this->form['query_args']['orderby'], 'distance ' ) ) {
-
-				$clauses['orderby'] = 'distance ASC, ' . $clauses['orderby'];
-
-				// Otherise, we order by the distance only.
-			} elseif ( 'distance' === $this->form['query_args']['orderby'] ) {
-
-				$clauses['orderby'] = ! empty( $this->form['query_args']['order'] ) ? 'distance ' . $this->form['query_args']['order'] : 'distance';
-			}
-		} else {
-
-			// if showing posts without location.
-			if ( $this->form['query_args']['gmw_args']['showing_objects_without_location'] ) {
-
-				// left join the location table into the query to display posts with no location as well.
-				$clauses['join']  .= " LEFT JOIN {$tjoin}";
-				$clauses['where'] .= " {$address_filters} ";
-
-			} else {
-
-				$clauses['join']  .= " INNER JOIN {$tjoin}";
-				$clauses['where'] .= " {$address_filters} AND ( gmw_locations.latitude != 0.000000 && gmw_locations.longitude != 0.000000 ) ";
-
-				if ( $where_clause_filter ) {
-					$clauses['where'] .= " AND gmw_locations.object_type = 'post'";
-				}
-			}
-		}
-
-		// modify the clauses.
-		$clauses = apply_filters( 'gmw_pt_location_query_clauses', $clauses, $this->form );
-
-		// make sure we have groupby to only pull posts one time.
-		if ( empty( $clauses['groupby'] ) ) {
-			$clauses['groupby'] = $wpdb->prefix . 'posts.ID';
-		}
-
-		// add having clause.
-		$clauses['groupby'] .= ' ' . $clauses['having'];
-
-		unset( $clauses['having'] );
-
-		return $clauses;
-	}
-
-	/**
 	 * Query results
 	 *
 	 * @return [type] [description]
@@ -279,23 +225,24 @@ class GMW_Posts_Locator_Form extends GMW_Form {
 
 		} else {
 
-			$post_types = ! empty( $this->form['search_form']['post_types'] ) ? $this->form['search_form']['post_types'] : 'post';
+			$post_types                        = ! empty( $this->form['search_form']['post_types'] ) ? $this->form['search_form']['post_types'] : 'post';
+			$this->form['form_values']['post'] = $post_types;
 		}
 
-		$this->query_cache_args['show_non_located'] = $this->enable_objects_without_location;
-
+		$tax_query = array();
+		
 		// tax query can be disable if a custom query is needed.
 		if ( apply_filters( 'gmw_enable_taxonomy_search_query', true, $this->form, $this ) ) {
-			$tax_args = ! empty( $this->form['form_values']['tax'] ) ? gmw_pt_get_tax_query_args( $this->form['form_values']['tax'], $this->form ) : array();
-		} else {
-			$tax_args = array();
+			//$tax_args = ! empty( $this->form['form_values']['tax'] ) ? gmw_pt_get_tax_query_args( $this->form['form_values']['tax'], $this->form ) : array();
+			$tax_query = gmw_generate_tax_query( $this->form );
 		}
 
-		$meta_args = false;
-
-		if ( empty( $this->form['get_per_page'] ) || -1 === $this->form['get_per_page'] ) {
+		if ( empty( $this->form['get_per_page'] ) ) {
 			$this->form['get_per_page'] = -1;
 		}
+
+		$this->query_cache_args['post_types']       = $post_types;
+		$this->query_cache_args['show_non_located'] = $this->enable_objects_without_location;
 
 		// query args.
 		$this->form['query_args'] = apply_filters(
@@ -303,19 +250,23 @@ class GMW_Posts_Locator_Form extends GMW_Form {
 			array(
 				'post_type'           => $post_types,
 				'post_status'         => array( 'publish' ),
-				'tax_query'           => apply_filters( 'gmw_pt_tax_query', $tax_args, $this->form ), // WPCS: slow query ok.
+				'tax_query'           => $tax_query, // WPCS: slow query ok.
 				'posts_per_page'      => $this->form['get_per_page'],
 				'paged'               => $this->form['paged'],
-				'meta_query'          => apply_filters( 'gmw_pt_meta_query', $meta_args, $this->form ), // WPCS: slow query ok.
 				'ignore_sticky_posts' => 1,
 				'orderby'             => 'distance',
 				'gmw_args'            => $this->query_cache_args,
+				// below we can save on performance when showing map only ( without the list of results ).
+				'no_found_rows'       => $this->form['display_list'] ? false : true,
+				//'fields'              => $this->form['display_list'] ? '*' : 'id=>parent',
 			),
 			$this->form,
 			$this
 		);
 
-		$this->form     = apply_filters( 'gmw_pt_form_before_posts_query', $this->form, $this );
+		$this->form = apply_filters( 'gmw_posts_locator_form_before_posts_query', $this->form, $this );
+		$this->form = apply_filters( 'gmw_pt_form_before_posts_query', $this->form, $this );
+
 		$internal_cache = GMW()->internal_cache;
 		$this->query    = false;
 
@@ -324,7 +275,7 @@ class GMW_Posts_Locator_Form extends GMW_Form {
 			// cache key.
 			$hash            = md5( wp_json_encode( $this->form['query_args'] ) );
 			$query_args_hash = 'gmw' . $hash . GMW_Cache_Helper::get_transient_version( 'gmw_get_object_post_query' );
-			$this->query     = get_transient( $query_args_hash );
+			$this->query     = get_option( $query_args_hash );
 		}
 
 		// look for query in cache.
@@ -351,7 +302,7 @@ class GMW_Posts_Locator_Form extends GMW_Form {
 				 *
 				 * @var [type]
 				 */
-				unset( $this->query->request );
+				unset( $this->query->request, $this->query->query_vars['search_orderby_title'] );
 
 				set_transient( $query_args_hash, $this->query, GMW()->internal_cache_expiration );
 			}
