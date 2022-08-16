b37f4906755036a28f55169e68396aef4282a4a0
Eyal Fitoussi <info@geomywp.com>
2022-08-15 14:36:19 -1000

    Load gmw-form stylesheet on all pages for now. This should be changed later and be loaded only when needed.

--- includes/gmw-enqueue-scripts.php
+++ includes/gmw-enqueue-scripts.php
@@ -64,9 +64,9 @@ function gmw_enqueue_form_stylesheet( $where = 'form' ) {
 		wp_enqueue_style( 'gmw-frontend' );
 	}
 
-	if ( in_array( $where, array( 'form', 'location_form', 'buddypress_directory', 'locations_dashboard', 'single_location', 'single_object_locations' ), true ) && ! wp_style_is( 'gmw-forms', 'enqueue' ) ) {
+	//if ( in_array( $where, array( 'form', 'location_form', 'buddypress_directory', 'locations_dashboard', 'single_location', 'single_object_locations' ), true ) && ! wp_style_is( 'gmw-forms', 'enqueue' ) ) {
 		wp_enqueue_style( 'gmw-forms' );
-	}
+	//}
 }
 add_action( 'gmw_element_loaded', 'gmw_enqueue_form_stylesheet' );
 
