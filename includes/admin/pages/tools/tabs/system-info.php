dfe3ea9e122dc938742d5d72ca5cf48c27ec0cce
Eyal Fitoussi <info@geomywp.com>
2023-03-08 22:30:58 -1000

    Enhance code.

--- includes/admin/pages/tools/tabs/system-info.php
+++ includes/admin/pages/tools/tabs/system-info.php
@@ -1,19 +1,23 @@
 <?php
 /**
  * GEO my WP Tools page - system info tab.
- * 
+ *
  * @since  2.5
  *
  * @author Eyal Fitoussi
+ *
+ * @package geo-my-wp
  */
 
-// Exit if accessed directly
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; 
+	exit;
 }
 
-include( dirname(__FILE__).'/../class-gmw-system-info.php' );
+require_once GMW_PATH . '/includes/admin/pages/tools/class-gmw-system-info.php';
 
-$system_info = new GMW_System_Info();
+if ( class_exists( 'GMW_System_Info' ) ) {
+	$system_info = new GMW_System_Info();
+}
 
 add_action( 'gmw_tools_system_info_tab', array( $system_info, 'output' ) );
