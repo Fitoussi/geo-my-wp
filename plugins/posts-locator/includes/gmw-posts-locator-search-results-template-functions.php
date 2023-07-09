2a70066e802bc1d1512d19facb7835bb49f3d829
Eyal Fitoussi <info@geomywp.com>
2023-07-09 22:20:45 +0300

    Tweak: enhance the gmw_pt_get_tax_query_args() function. Tweak: new function gmw_pt_get_tax_query_args_multiple_pt() that is not being used at the moment.

--- plugins/posts-locator/includes/gmw-posts-locator-search-results-template-functions.php
+++ plugins/posts-locator/includes/gmw-posts-locator-search-results-template-functions.php
@@ -25,12 +25,25 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function gmw_pt_get_tax_query_args( $tax_args = array(), $gmw = array() ) {
 
-	// Abort if multiple post types selected.
-	if ( empty( $gmw['search_form']['post_types'] ) || 1 !== count( $gmw['search_form']['post_types'] ) || empty( $gmw['search_form']['taxonomies'] ) ) {
+	if ( ! empty( $gmw['form_values']['post'] ) ) {
+
+		$post_types = $gmw['form_values']['post'];
+
+	} elseif ( ! empty( $gmw['search_form']['post_types'] ) ) {
+
+		$post_types = $gmw['search_form']['post_types'];
+
+	} else {
+
 		return array();
 	}
 
-	$output = array( 'relation' => 'AND' );
+	if ( 1 !== count( $post_types ) || empty( $gmw['search_form']['taxonomies'] ) ) {
+		return array();
+	}
+
+	$post_type = $post_types[0];
+	$tax_query = array( 'relation' => 'AND' );
 
 	// Loop through taxonomies in the search form settings.
 	foreach ( $gmw['search_form']['taxonomies'] as $taxonomy => $taxonomy_args ) {
@@ -40,8 +53,8 @@ function gmw_pt_get_tax_query_args( $tax_args = array(), $gmw = array() ) {
 			continue;
 		}
 
-		// Skip taxonomy if not below to the selected post type.
-		if ( ! in_array( $gmw['search_form']['post_types'][0], $taxonomy_args['post_types'], true ) ) {
+		// Skip taxonomy if not belong to the selected post type.
+		if ( ! in_array( $post_type, $taxonomy_args['post_types'], true ) ) {
 			continue;
 		}
 
@@ -57,7 +70,7 @@ function gmw_pt_get_tax_query_args( $tax_args = array(), $gmw = array() ) {
 		// Query taxonomy from the search form.
 		if ( $tax_exists ) {
 
-			$output[] = array(
+			$tax_query[] = array(
 				'taxonomy' => $taxonomy,
 				'field'    => 'id',
 				'terms'    => $tax_values,
@@ -72,7 +85,7 @@ function gmw_pt_get_tax_query_args( $tax_args = array(), $gmw = array() ) {
 			// include terms.
 			if ( ! empty( $taxonomy_args['include'] ) ) {
 
-				$output[] = array(
+				$tax_query[] = array(
 					'taxonomy' => $taxonomy,
 					'field'    => 'id',
 					'terms'    => is_array( $taxonomy_args['include'] ) ? $taxonomy_args['include'] : explode( ',', $taxonomy_args['include'] ),
@@ -83,7 +96,7 @@ function gmw_pt_get_tax_query_args( $tax_args = array(), $gmw = array() ) {
 			// exclude terms.
 			if ( ! empty( $taxonomy_args['exclude'] ) ) {
 
-				$output[] = array(
+				$tax_query[] = array(
 					'taxonomy' => $taxonomy,
 					'field'    => 'id',
 					'terms'    => is_array( $taxonomy_args['exclude'] ) ? $taxonomy_args['exclude'] : explode( ',', $taxonomy_args['exclude'] ),
@@ -93,11 +106,155 @@ function gmw_pt_get_tax_query_args( $tax_args = array(), $gmw = array() ) {
 		}
 
 		// extend the taxonomy query.
-		$output = apply_filters( 'gmw_' . $gmw['prefix'] . '_query_taxonomy', $output, $taxonomy, $tax_values, $gmw );
+		$tax_query = apply_filters( 'gmw_' . $gmw['prefix'] . '_query_taxonomy', $tax_query, $taxonomy, $tax_values, $gmw );
 	}
 
-	return ! empty( $output[0] ) ? $output : array();
+	return ! empty( $tax_query[0] ) ? $tax_query : array();
 }
+
+/**
+ * Below is an attepms to create a WP_Query with multiple post types and multiple taxonomies.
+ *
+ * This function only partially working. The issue is when selecting multiple post types but selecting the taxonomiy/ies of a single post type only.
+ *
+ * In this case, the query ignores the post types that have no taxonomies selected and exclude those post types from the search results.
+ *
+ * One attempt to solve this was to generate a tax_query for each of those non-selected post types and pass all the terms ( usign get_terms ) to the query with the "IN" selector.
+ *
+ * This works for posts that have at least one category selected, but will exclude posts without any category.
+ *
+ * Another issue is that the query ignores post types that don't have taxonomies, which in this case the solution metnioedn does not work.
+ *
+ * Maybe will look for another solution again in the future.
+ */
+/*function gmw_pt_get_tax_query_args_multiple_pt( $tax_args = array(), $gmw = array() ) {
+
+	$output     = array( 'relation' => 'AND' );
+	$post_types = ! empty( $gmw['form_values']['post'] ) ? $gmw['form_values']['post'] : $gmw['search_form']['post_types'];
+	$pt_taxes   = array();
+	$tax_query  = array();
+
+	// Loop through taxonomies in the search form settings.
+	foreach ( $gmw['search_form']['taxonomies'] as $taxonomy => $taxonomy_args ) {
+
+		// Skip if taxonomy is disabled.
+		if ( empty( $taxonomy_args['style'] ) || 'disable' === $taxonomy_args['style'] || 'disabled' === $taxonomy_args['style'] ) {
+			continue;
+		}
+
+		if ( empty( $taxonomy_args['post_types'] ) ) {
+			continue;
+		}
+
+		$post_type = array_intersect( $taxonomy_args['post_types'], $post_types );
+
+		if ( empty( $post_type ) ) {
+			continue;
+		}
+
+		$post_type = $post_type[0];
+
+		if ( ! isset( $tax_query[ $post_type ] ) ) {
+			$tax_query[ $post_type ] = array();
+		}
+
+		$tax_exists = false;
+		$tax_values = array();
+
+		// Check if taxonomy term/s were selected in the search form.
+		if ( ! empty( $tax_args[ $taxonomy ] ) && array_filter( $tax_args[ $taxonomy ] ) ) {
+			$tax_exists = true;
+			$tax_values = $tax_args[ $taxonomy ];
+		}
+
+		// Query taxonomy from the search form.
+		if ( $tax_exists ) {
+
+			$tax_query[ $post_type ][] = array(
+				'taxonomy' => $taxonomy,
+				'field'    => 'id',
+				'terms'    => $tax_values,
+				'operator' => 'IN',
+			);
+		}
+
+		// Query include/exclude terms.
+		// Only if taxonomy was not selected in the search form or is set to pre-defined.
+		if ( $gmw['submitted'] && ( ! $tax_exists || 'pre_defined' === $taxonomy_args['style'] ) ) {
+
+			// include terms.
+			if ( ! empty( $taxonomy_args['include'] ) ) {
+
+				$tax_query[ $post_type ][] = array(
+					'taxonomy' => $taxonomy,
+					'field'    => 'id',
+					'terms'    => is_array( $taxonomy_args['include'] ) ? $taxonomy_args['include'] : explode( ',', $taxonomy_args['include'] ),
+					'operator' => 'IN',
+				);
+			}
+
+			// exclude terms.
+			if ( ! empty( $taxonomy_args['exclude'] ) ) {
+
+				$tax_query[ $post_type ][] = array(
+					'taxonomy' => $taxonomy,
+					'field'    => 'id',
+					'terms'    => is_array( $taxonomy_args['exclude'] ) ? $taxonomy_args['exclude'] : explode( ',', $taxonomy_args['exclude'] ),
+					'operator' => 'NOT IN',
+				);
+			}
+		}
+
+		if ( ! empty( $tax_query[ $post_type ] ) ) {
+
+			$pt_taxes[] = $post_type;
+
+			$tax_query[ $post_type ]['relation'] = 'AND';
+
+		} else {
+			unset( $tax_query[ $post_type ] );
+		}
+	}
+
+	// Abort if no taxonomy terms selected in the search form.
+	if ( empty( $tax_query ) ) {
+		return array();
+	}
+
+	foreach ( $post_types as $post_type ) {
+
+		if ( ! in_array( $post_type, $pt_taxes, true ) ) {
+
+			$taxonomies = get_object_taxonomies( $post_type );
+
+			$tax_query[ $post_type ]['relation'] = 'OR';
+
+			foreach ( $taxonomies as $pt_tax ) {
+
+				$args = array(
+					'taxonomy'   => $pt_tax,
+					'hide_empty' => false,
+					'fields'     => 'ids',
+				);
+
+				$all_terms = get_terms( $args );
+
+				$tax_query[ $post_type ][] = array(
+					'taxonomy' => $pt_tax,
+					'field'    => 'id',
+					'terms'    => $all_terms,
+					'operator' => 'IN',
+				);
+			}
+		}
+	}
+
+	if ( ! empty( $tax_query ) ) {
+		$tax_query['relation'] = 'OR';
+	}
+
+	return $tax_query;
+}*/
 
 /**
  * Query pre-defined taxonomies.
